5a36773015cbdf0c4b216afd6ffb3e5775eeeb9f
Fab <42979283+fchoi1@users.noreply.github.com>
2023-09-13 01:02:40 -0400

    Time: 42 ms (37.42%), Space: 16.3 MB (28.99%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if len(s) != len(goal):
-            return False
+        
 
         for _ in range(len(s)):
             if s == goal:
