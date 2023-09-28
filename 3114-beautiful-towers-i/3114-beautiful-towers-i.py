a4b837720281ef20b85c5492500606bd6e29ebf3
Fab <42979283+fchoi1@users.noreply.github.com>
2023-09-28 03:30:27 -0400

    Time: 87 ms (93.52%), Space: 17 MB (6.21%) - LeetHub

--- 3114-beautiful-towers-i/3114-beautiful-towers-i.py
+++ 3114-beautiful-towers-i/3114-beautiful-towers-i.py
@@ -1,84 +1,41 @@
 class Solution:
     def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
+        def generatePrefixSum(hieghts: List) -> List:
+            print(hieghts)
+            prefixList = []
+            stack = []
+            prev = hieghts[0]
+            for i,h in enumerate(hieghts):
+                if h >= prev:
+                    if not prefixList:
+                        prefixList.append(h)
+                    else:
+                        prefixList.append(h + prefixList[-1])
+                    prev = h
+                    stack.append((h,i))
+                    continue
+                while h < prev:
+                    if not stack:
+                        prev = h
+                        index = None
+                        break
+                    prev, index = stack.pop()
+                if index is not None:
+                    stack.append((prev,index))
+                    prefixList.append(h*(i-index) + prefixList[index])
+                else:
+                    prefixList.append(h*(i+1))
+                prev = h
+                stack.append((h,i))
+            return prefixList
 
         N = len(maxHeights)
-        forward = []
-        backward = []
-        stack = []
-        prev = maxHeights[0]
-        index = None
-    
-        for i,h in enumerate(maxHeights):
-           
-            if h >= prev:
-                if not forward:
-                    forward.append(h)
-                else:
-                    forward.append(h + forward[-1])
-                prev = h
-                stack.append((h,i))
-                continue
-            # print(i, stack)
-            while h < prev:
-                if not stack:
-                    prev = h
-                    index = None
-                    break
-                prev, index = stack.pop()
-            # print(i, 'after', prev, h, index)
-            if index is not None:
-                # print('has index',i,index, prev, h,h*(i-index),forward[index] )
-                stack.append((prev,index))
-                forward.append(h*(i-index) + forward[index])
-            else:
-                forward.append(h*(i+1))
-            prev = h
-            stack.append((h,i))
-
-        stack = []
-        prev = maxHeights[-1]
-        index = None
-        for i,h in enumerate(maxHeights[::-1]):
-           
-            if h >= prev:
-                if not backward:
-                    backward.append(h)
-                else:
-                    backward.append(h + backward[-1])
-                prev = h
-                stack.append((h,i))
-                continue
-
-            while h < prev:
-                if not stack:
-                    prev = h
-                    index = None
-                    break
-                prev, index = stack.pop()
-            
-            if index is not None:
-                stack.append((prev,index))
-                backward.append(h*(i-index) + backward[index])
-            else:
-                backward.append(h*(i+1))
-            prev = h
-            stack.append((h,i))
-
-     
-        # forward[0] = 0
-        # backward.append(0)
+        forward = generatePrefixSum(maxHeights)
+        backward = generatePrefixSum(maxHeights[::-1])
         backward.reverse()
-        print(forward, backward)
-
         maxSum = forward[-1]
-    
         for i in range(N):
-            if i== 0:
-                maxSum = max(maxSum,backward[i])
-
-            else:
-                maxSum = max(maxSum,forward[i-1] + backward[i])
-
+            maxSum = max(maxSum, backward[i] if i == 0 else forward[i - 1] + backward[i])
         return maxSum
             
         
