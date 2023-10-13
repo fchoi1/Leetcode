1b91d9d13046451ddcaad26f2e6820b0a0166782
Fab <42979283+fchoi1@users.noreply.github.com>
2023-10-13 18:49:49 -0400

    Time: 43 ms (71.73%), Space: 16.4 MB (81.09%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,21 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         numSet = set(nums)
-
         result = []
-        N = len(nums)
-        def generate(numSet, currList):
-            if len(currList) >= N:
-                result.append(currList)
+        def generate(currList):
+            if len(currList) >= len(nums):
+                result.append(currList[:])
                 return
             
-            for n in numSet:
-                newSet = set(numSet)
-                newSet.remove(n)
-                newList = currList[:]
-                newList.append(n)
-                generate(newSet, newList)
+            for n in nums:
+                if n not in currList:
+                    currList.append(n)
+                    generate(currList)
+                    currList.pop()
 
-        generate(numSet, [])
+        generate([])
         return result
         
