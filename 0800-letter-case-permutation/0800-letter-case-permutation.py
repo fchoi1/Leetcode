0838d1973a7da5cc9991d4b9ea7fcc67509d31fb
Fab <42979283+fchoi1@users.noreply.github.com>
2023-10-18 15:53:19 -0400

    Time: 65 ms (31.89%), Space: 17.7 MB (44.88%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.py
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -1,31 +1,22 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        result = []
+        ans = []
 
-        def backtrack(string, i):
+        def backtrack(i, path):
             if i == len(s):
-                result.append("".join(string))
-                return
+                ans.append("".join(path))
+                return 
 
-            if s[i].isdigit():
-                backtrack(string + [s[i]], i+1)
-            else:
-                backtrack(string + [s[i].upper()], i+1)
-                backtrack(string + [s[i].lower()], i+1)
+            path.append(s[i].lower())
+            backtrack(i + 1, path)
+            path.pop()
 
-            
+            if s[i].isalpha():
+                path.append(s[i].upper())
+                backtrack(i + 1, path)
+                path.pop()
 
-        def generate(string, index):
-            if len(string) == len(s):
-                result.append(string)
-                return
-            if s[index].isdigit():
-                generate(string+s[index], index+1)
-            else:
-                generate(string+s[index].upper(), index+1)
-                generate(string+s[index].lower(), index+1)
+        backtrack(0, [])
 
-        # generate("",0)
-        backtrack([],0)
-        return result
+        return ans
         
