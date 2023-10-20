be0c8207151cfdf9b1c2f93406697c7e4ad61df1
Fab <42979283+fchoi1@users.noreply.github.com>
2023-10-20 11:01:32 -0400

    Time: 72 ms (31.78%), Space: 16.4 MB (75.98%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,14 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        charNote = [0] * 26
         charMag = [0] * 26
         for char in magazine:
             charMag[ord(char)- ord('a')] +=1
 
         for char in ransomNote:
             index = ord(char)- ord('a')
-            charNote[index] +=1
-            if charNote[index] > charMag[index]:
+            charMag[index] -=1
+            if charMag[index] < 0:
                 return False
         return True
         
