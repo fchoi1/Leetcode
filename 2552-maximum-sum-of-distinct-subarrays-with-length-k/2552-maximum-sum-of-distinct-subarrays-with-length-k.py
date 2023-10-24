2adfc6b00130186b251dfffb29681158d9cca30f
Fab <42979283+fchoi1@users.noreply.github.com>
2023-10-23 22:08:55 -0400

    Time: 637 ms (68.13%), Space: 34.5 MB (17.34%) - LeetHub

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        dupe = defaultdict(int)
+        currSum = total = 0
+        for i in range(k):
+            dupe[nums[i]]+=1
+            currSum += nums[i]
+        if len(dupe) == k:
+            total = currSum
+        
+        for i in range(len(nums)-k):
+            # print(currSum, nums[i], nums[i+k], dupe)
+            currSum -= nums[i]
+            currSum += nums[i+k]
+            dupe[nums[i+k]]+=1
+            dupe[nums[i]]-=1
+            if dupe[nums[i]] <= 0:
+                del dupe[nums[i]]
+            if len(dupe) == k:
+                total = max(total,currSum)
+        
+        return total
+
+
+
+            
+
+        
