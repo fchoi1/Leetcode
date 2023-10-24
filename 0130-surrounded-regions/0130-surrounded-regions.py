a1a8fe086af9bb1047f2d0fe4752ef67d91df660
Fab <42979283+fchoi1@users.noreply.github.com>
2023-10-24 01:42:42 -0400

    Time: 142 ms (40.16%), Space: 18.4 MB (39.16%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,18 +1,13 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
         visited = set()
         dirMap = [(0,-1),(0,1),(1,0),(-1,0)]
-        noFlip = set()
         m,n = len(board[0]), len(board)
 
         def bfs(coords):
             if coords in visited:
                 return
             visited.add(coords)
-            noFlip.add(coords)
             x, y = coords
             for dx, dy in dirMap:
                 if 0<= x+dx<m and 0 <= y+dy<n:
@@ -34,5 +29,5 @@ class Solution:
 
         for y in range(n):
             for x in range(m):
-                if board[y][x] == "O" and (x,y) not in noFlip:
+                if board[y][x] == "O" and (x,y) not in visited:
                     board[y][x] = "X" 
