cfcd3758c2a1d8fb3f28691998d47af2dac5539d
Fab <42979283+fchoi1@users.noreply.github.com>
2023-12-01 20:44:43 -0500

    Time: 106 ms (74.24%), Space: 17 MB (13.86%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,17 +1,19 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        
-        def countLetters(word):
-            charList = [0] * 26
+        charList = [0] * 26
+        count = 0 
+        for char in chars:
+            charList[ord(char) - ord('a')] += 1
+            
+        for word in words:
+            wordList = [0] * 26
             for char in word:
-                charList[ord(char) - ord('a')] += 1
-            return charList
-        
-        def isWordinChars(word, charList):
-            wordList = countLetters(word)
-            return all(wordList[i] <= charList[i] for i in range(26))
-        
-        charsList = countLetters(chars)
-        return sum(len(word) for word in words if isWordinChars(word,charsList))
+                index = ord(char) - ord('a')
+                wordList[index] += 1
+                if wordList[index] > charList[index]:
+                    break
+            else:
+                count += len(word)
+        return count
                     
                 
