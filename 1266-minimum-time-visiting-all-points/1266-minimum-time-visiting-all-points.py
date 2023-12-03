e6a538a116a06a36dd5a59295980f5e8dda1cb8f
Fab <42979283+fchoi1@users.noreply.github.com>
2023-12-03 14:03:19 -0500

    Time: 64 ms (55.91%), Space: 16.5 MB (30.54%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -1,15 +1,9 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        
-        # check on same diag y = mx + b   b=(y-x)
-        # move towards diag
         time = 0
-        currX, currY = points[0]
-        for x,y in points:
-            if currX == x and currY == y:
-                continue
-            dx = abs(x - currX)
-            dy = abs(y - currY)
+        for i,[x,y] in enumerate(points[1:]):
+            dx = abs(x - points[i][0])
+            dy = abs(y - points[i][1])
             h = min(dx, dy)
             time += h + abs(dx-dy)
             currX, currY = x,y
