5a5369a0f7981ee93697d7f74db42a41bfd4fb90
Fab <42979283+fchoi1@users.noreply.github.com>
2023-12-03 22:37:54 -0500

    Time: 57 ms (5.11%), Space: 16.4 MB (24.76%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
@@ -2,7 +2,6 @@ class Solution:
     def largestGoodInteger(self, num: str) -> str:
         total = None
         for i,c in enumerate(num[:-2]):
-            print(i, c, num[i:i+3])
             if all(x == c for x in num[i:i+3]) and num[i:i+3].isdigit():
                 if total == None:
                     total = int(num[i:i+3])
