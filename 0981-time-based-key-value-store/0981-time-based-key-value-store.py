19635ace78b283b66592a6ff1dfa692f2e0403a2
Fab <42979283+fchoi1@users.noreply.github.com>
2023-12-12 20:22:43 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,31 +1,37 @@
 class TimeMap:
 
     def __init__(self):
-        self.data = {}
+        self.data = {}        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
+        print(self.data)
         if key not in self.data:
-            self.data[key] = [{"val": value, "times":[timestamp]}]
+            self.data[key] = [(value, timestamp)]
         else:
-            self.data[key][-1]["times"].append(timestamp - 1)
-            self.data[key].append({"val": value, "times":[timestamp]})
-            
+            self.data[key].append((value, timestamp))
+    
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.data:
-            return ""
-        if timestamp >= self.data[key][-1]["times"][0]:
-            return self.data[key][-1]["val"]
-        
-        minIndex = 0
-        maxIndex = len(self.data[key])
-        
-        while maxIndex > minIndex:
-            half = (maxIndex + minIndex) // 2
-            if timestamp < self.data[key][half]["times"][0]:
-                maxIndex = half
-            elif timestamp > self.data[key][half]["times"][1]:
-                minIndex = half
-            else:
-                return self.data[key][half]["val"]
-        return ""
+            return  ""
+        ts = self.data[key]
+        left = 0
+        right = len(ts) - 1
+        #  1 4 6  -> timestamp = 3
+        # left = 0, right = 2 half = 1
 
+        while left < right:
+            half = (left + right) // 2
+            if timestamp < ts[half][1]:
+                right = half
+            if timestamp >= ts[half][1]:
+                left = half + 1
+        return ts[left][0]
+
+        
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
