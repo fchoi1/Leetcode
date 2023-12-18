1fb49c58296577e58a75adde5d997bc4d4dc0c64
Fab <42979283+fchoi1@users.noreply.github.com>
2023-12-18 17:13:56 -0500

    Time: 91 ms (27.62%), Space: 16.2 MB (78.01%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        W = len(matrix[0])
+        H = len(matrix)
+        def checkDiag(pos):
+            x, y = pos
+            val = matrix[y][x]
+            while 0 <= x < W and 0 <= y < H:
+                if val != matrix[y][x]:
+                    return False
+                x += 1
+                y += 1
+            return True
+                
+        # first col
+        for i in range(H):
+            if not checkDiag((0,i)):
+                return False
+
+        # first row
+        for i in range(W):
+            if not checkDiag((i,0)):
+                return False
+        return True
+            
+        
