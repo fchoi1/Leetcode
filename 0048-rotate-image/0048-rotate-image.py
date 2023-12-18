f7c245987857b3f7da57d0017735a9511337d8fb
Fab <42979283+fchoi1@users.noreply.github.com>
2023-12-18 16:02:55 -0500

    Time: 33 ms (96.4%), Space: 16.4 MB (38.15%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,10 +1,8 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
- 
-        for j, row in enumerate(matrix):
-            for i in range(j,len(row)):
-                matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
 
-        for i, row in enumerate(matrix):
+
+        transposed = [list(x) for x in zip(*matrix)]
+        for i, row in enumerate(transposed):
             matrix[i] = row[::-1]
         
