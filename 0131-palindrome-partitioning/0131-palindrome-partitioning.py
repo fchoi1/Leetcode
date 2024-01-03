785f407e1b7ce2551cc9ae81b3dc37b46985b975
Fab <42979283+fchoi1@users.noreply.github.com>
2024-01-02 23:30:14 -0500

    Time: 489 ms (90.01%), Space: 36.1 MB (13.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,36 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        
-        def checkPartitions(parts):
-            parts = [0] + parts + [len(s)]
-            arr = []
-            for i in range(0, len(parts)-1):
-                start = parts[i]
-                end = parts[i+1]
-                arr.append(s[start:end])
-                if not isPali(s[start:end]):
-                    return False, None
-                prev = i
-            return  True, arr
-     
+        part = []
 
         def isPali(string):
             extra = (len(string) % 2 == 0) 
             return string[:len(string) // 2] == string[len(string):len(string) // 2-extra:-1]
 
-        def backtrack(index,parts):
-            isValid,arr = checkPartitions(parts)
-            if isValid:
-                res.append(arr)
-                
-            if index == len(s):
-                return
+        def backtrack(i):
 
-            for i in range(index, len(s)):
-                parts.append(i)
-                backtrack(i+1, parts)
-                parts.pop()
-            pass
-        backtrack(1,[])
+            if i == len(s):
+                res.append(part[:])
+                return
+            for j in range(i, len(s)):
+                if isPali(s[i: j+1]):
+                    part.append(s[i:j+1])
+                    backtrack(j+1)
+                    part.pop()
+        
+        backtrack(0)
         return res
