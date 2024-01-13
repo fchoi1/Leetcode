117efad074bec75b83fea8fc42ab8d469a8550e5
Fab <42979283+fchoi1@users.noreply.github.com>
2024-01-12 23:38:08 -0500

    Time: 37 ms (89.91%), Space: 17.7 MB (18.73%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,11 +1,4 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
+from collections import deque
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
@@ -14,23 +7,21 @@ class Solution:
         nodeList = {}
         seen = set()
         start = node.val
-        q = [node]
+        q = deque([node])
         while q:
-            temp = []
-            for node in q:
-                val = node.val
-                if val in seen:
-                    continue
-                seen.add(val)
-                newNeighbors = []
-                for neighbor in node.neighbors:
-                    if neighbor.val not in nodeList:
-                        nodeList[neighbor.val] = Node(neighbor.val)
-                    newNeighbors.append(nodeList[neighbor.val])
-                    temp.append(neighbor)
-                if node.val in nodeList:
-                    nodeList[val].neighbors = newNeighbors
-                else:
-                    nodeList[val] = Node(val, newNeighbors)
-            q = temp
+            node = q.popleft()
+            val = node.val
+            if val in seen:
+                continue
+            seen.add(val)
+            newNeighbors = []
+            for neighbor in node.neighbors:
+                if neighbor.val not in nodeList:
+                    nodeList[neighbor.val] = Node(neighbor.val)
+                newNeighbors.append(nodeList[neighbor.val])
+                q.append(neighbor)
+            if node.val in nodeList:
+                nodeList[val].neighbors = newNeighbors
+            else:
+                nodeList[val] = Node(val, newNeighbors)
         return nodeList[start]   
