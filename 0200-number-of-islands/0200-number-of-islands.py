fcf1cee029adf20d6f9d87c3c36dd9f317c5e144
Fab <42979283+fchoi1@users.noreply.github.com>
2024-01-17 11:33:22 -0500

    Time: 289 ms (49.71%), Space: 23.6 MB (43.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,25 +4,19 @@ class Solution:
       visited = {}
       m = len(grid)
       n = len(grid[0])
-      #add zeros at edges
       
-      for row in range(m):
-        grid[row].insert(0, 0)
-        grid[row].append(0)
-      
-      grid.insert(0, [0]*(n+2))
-      grid.append([0]*(n+2))
-      
+      def inRange(x,y):
+          return 0 <= x < m and 0 <= y < n
       def foundIsland(row, col, grid):
         directions = [(row-1, col), (row, col-1), (row+1,col), (row,col+1)]
         for (x, y) in directions:
-          if grid[x][y] == "1" and f'{x},{y}' not in visited:
-            visited[f'{x},{y}'] = True
-            foundIsland(x, y, grid)
+          if inRange(x,y) and grid[x][y] == "1" and f'{x},{y}' not in visited:
+                visited[f'{x},{y}'] = True
+                foundIsland(x, y, grid)
         
       count = 0
-      for row in range(1,m+1):
-        for col in range(1,n+1):
+      for row in range(m):
+        for col in range(n):
             if grid[row][col] == "1" and f'{row},{col}' not in visited:
                 foundIsland(row, col, grid)
                 count += 1
