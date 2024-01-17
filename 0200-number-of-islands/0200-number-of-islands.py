945cfd24f8a7f2b8bd45df21a3482151fc416c10
Fab <42979283+fchoi1@users.noreply.github.com>
2024-01-17 11:34:09 -0500

    Time: 271 ms (65.8%), Space: 27.8 MB (11.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
   
-      visited = {}
+      visited = set()
       m = len(grid)
       n = len(grid[0])
       
       def inRange(x,y):
           return 0 <= x < m and 0 <= y < n
+
       def foundIsland(row, col, grid):
         directions = [(row-1, col), (row, col-1), (row+1,col), (row,col+1)]
         for (x, y) in directions:
-          if inRange(x,y) and grid[x][y] == "1" and f'{x},{y}' not in visited:
-                visited[f'{x},{y}'] = True
+          if inRange(x,y) and grid[x][y] == "1" and (x,y) not in visited:
+                visited.add((x,y))
                 foundIsland(x, y, grid)
         
       count = 0
       for row in range(m):
         for col in range(n):
-            if grid[row][col] == "1" and f'{row},{col}' not in visited:
+            if grid[row][col] == "1" and (row,col) not in visited:
                 foundIsland(row, col, grid)
                 count += 1
       return count
