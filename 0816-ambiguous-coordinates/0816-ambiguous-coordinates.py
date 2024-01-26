1569783e4175e049aabdbca023dda2fa45c559a3
Fab <42979283+fchoi1@users.noreply.github.com>
2024-01-25 20:52:10 -0500

    Time: 56 ms (46.34%), Space: 16.7 MB (78.05%) - LeetHub

---
+++ 0816-ambiguous-coordinates/0816-ambiguous-coordinates.py
@@ -0,0 +1,43 @@
+class Solution:
+    def ambiguousCoordinates(self, s: str) -> List[str]:
+
+        strNum = s[1:-1]
+
+        def getValidCount(strFloat, seen):
+            result = []
+            if float(strFloat) == 0 and len(strFloat) > 1:
+                return []
+
+            if float(strFloat) not in seen and (len(strFloat) == 1 or strFloat[0] != '0'):
+                seen.add(float(strFloat))
+                result.append(strFloat)
+
+            for i in range(1, len(strFloat)):
+                left = strFloat[:i]
+                right = strFloat[i:]
+
+                # check trailing zeros
+                if len(left) > 1 and left[0] == '0' or right[-1] == '0' :
+                    return result
+
+                if float(left+"."+right) not in seen:
+                    seen.add(float(left+"."+right))
+                    result.append(left+"."+right)
+
+            return result
+
+
+        leftSeen = set()
+        rightSeen = set()
+        result = []
+        for i in range(1, len(strNum)):
+            left = strNum[:i]
+            right = strNum[i:]
+            leftCombo = getValidCount(left, leftSeen)
+            rightCombo = getValidCount(right, rightSeen)
+            for l in leftCombo:
+                for r in rightCombo:
+                    result.append(f"({l}, {r})")
+        return result
+            
+ 
