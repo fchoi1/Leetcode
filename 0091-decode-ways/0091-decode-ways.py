42a27f691debc190489a47e4d12c77c267e5683b
Fab <42979283+fchoi1@users.noreply.github.com>
2024-01-27 21:53:05 -0500

    Time: 38 ms (54.96%), Space: 16.6 MB (89.4%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,27 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == "0":
+        if s[0] == "0":
             return 0
-        if len(s) == 1:
-            return 1
-
         dp = [1]
-        if s[1] == '0' and s[0] not in '12':
-            return 0
-        elif s[1] == '0' and s[0] in '12' or not 0 < int(s[:2]) <= 26 :
-            dp.append(1)
-        else:
-            dp.append(2)
-
-        prev = s[1]
-        for char in s[2:]:
+        prev = s[0]
+        for char in s[1:]:
             num = int(prev+char)
             if num in [10, 20]:
-                dp.append(dp[-2])
+                if len(dp) == 1:
+                    dp.append(1)
+                else:
+                    dp.append(dp[-2])
             elif char == '0':
                 return 0
             elif 0 < num <= 26 and prev != '0':
-                dp.append(dp[-1] + dp[-2])
+                if len(dp) == 1:
+                    dp.append(2)
+                else:
+                    dp.append(dp[-1] + dp[-2])
             else:
                 dp.append(dp[-1])
             prev = char
