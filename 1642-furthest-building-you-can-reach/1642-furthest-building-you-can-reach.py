51c948a6d6b1766f9621e8b562ddbad32dc7ce4d
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-17 13:54:47 -0500

    Time: 404 ms (96.28%), Space: 31 MB (89.78%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,24 +1,16 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        # use bricks for low elevation
-        # use ladders for more
-
-        # keep heap of ladders
-        # use ladders on tallest gap
-        # use bricks on lower gaps
         heap = []
         curr = heights[0]
-        diffs = [0]
         for i, h in enumerate(heights):
             diff = h - curr
             if diff > 0:
                 if len(heap) < ladders:
                     heapq.heappush(heap, diff)
                 elif len(heap) == ladders:
-                    if len(heap) == 0:
+                    if len(heap) == 0 or diff < heap[0]:
                         bricks -= diff
-                    elif heap and diff < heap[0]:
-                        bricks -= diff
+                  
                     else:
                         bricks -= heapq.heappop(heap)
                         heapq.heappush(heap, diff)
