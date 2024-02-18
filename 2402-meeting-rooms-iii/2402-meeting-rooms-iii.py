166fa6f79528e8555c671fca7f685f94cef4f55b
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-17 22:18:57 -0500

    Time: 1245 ms (43.66%), Space: 62.4 MB (78.07%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        meetings.sort(key=lambda x: (x[0], x[1]))
+        rooms = list(range(n))
+        roomCount = [0] * n
+        ongoing = []
+        maxCount = roomNum = 0
+        for start, end in meetings:
+            while ongoing and start >= ongoing[0][0]:
+                _, num = heapq.heappop(ongoing)
+                heapq.heappush(rooms,num)
+
+            if len(rooms) != 0:
+                avail = heapq.heappop(rooms)
+                heapq.heappush(ongoing,(end, avail))
+            else:
+                prevEnd, avail = heapq.heappop(ongoing)
+                heapq.heappush(ongoing,(prevEnd + end - start, avail))
+
+            roomCount[avail] += 1
+            if roomCount[avail] > maxCount:
+                maxCount, roomNum = roomCount[avail],avail
+            elif roomCount[avail] == maxCount:
+                roomNum = min(roomNum, avail)
+        return roomNum
+        
