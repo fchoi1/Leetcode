e223bcb202f1effb79aef3f194f7c06b8f469614
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-17 21:39:36 -0500

    Time: 74 ms (67.21%), Space: 16.7 MB (91.67%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,27 +1,22 @@
-
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        slopes = defaultdict(set)
-        maxCount = 1
-        currSlope = None
-        for i, (x1, y1) in enumerate(points[:-1]):
-            for x2, y2 in points[i+1:]:
-                if y1 == y2:
-                    m = 0
-                    b = y1
-                elif x1 == x2: 
-                    m = float('inf')
-                    b = x1
-                else:
-                    m = (y1 - y2) / (x1-x2)
-                    b = y1 - m * x1
-                s = (m,b)
-                slopes[s].add((x1,y1))
-                slopes[s].add((x2,y2))
-                if len(slopes[s]) > maxCount:
-                    maxCount = len(slopes[s]) 
-                    currSlope = s
-        return maxCount 
+        n = len(points)
+        if n <= 2:
+            return n # There is always a line that passes through 2 points
+        maxi = 1 # In starting maxi as 1 as one point always lie on a staright line 
+        for ind, poi1 in enumerate(points): # Using enumerate to get index as well as the num 
+            slopeM = defaultdict(int) # Making a dictionary to keep track of all types of slope 
+            for j , poi2 in enumerate(points[ind+1:]): # getting x2 and y2 as point2 therefore starting from index of previous +1
+                slope = self.slope(poi1,poi2) # finding the slope using point1 and 2
+                slopeM[slope] += 1
+                maxi = max(slopeM[slope],maxi) # Updating the maximum 
+        return maxi+1
 
-
+    def slope(self,x,y):
+        x1, y1 = x
+        x2, y2 = y
+        if x1-x2 == 0:
+            return 
+        return (y1-y2)/(x1-x2) # Slope of a line
+    
         
