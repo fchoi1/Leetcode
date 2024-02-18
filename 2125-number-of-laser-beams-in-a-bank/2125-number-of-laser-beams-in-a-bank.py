52efeae54279d993b5759ed90fa3c5c19072c260
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-17 22:35:17 -0500

    Time: 99 ms (65.16%), Space: 18.9 MB (97.51%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,11 +1,11 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
         lasers = 0
-        prevLasers = sum(x == '1' for x in bank[0])        
+        prevLasers = bank[0].count('1')    
         for row in bank[1:]:
             if int(row) == 0:
                 continue
-            currLasers = sum(x == '1' for x in row) 
+            currLasers = row.count('1')
             lasers += currLasers* prevLasers
             prevLasers = currLasers
         return lasers
