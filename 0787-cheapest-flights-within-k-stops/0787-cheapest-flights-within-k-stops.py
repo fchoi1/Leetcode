53581c28e3f49e33b84779ed1e6bd98b2661b00b
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-23 02:54:59 -0500

    Time: 88 ms (91.15%), Space: 18 MB (63.42%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -8,16 +8,25 @@ class Solution:
 
         q = deque([(src, 0)])
         steps = 0
+        cheapest = float('inf')
         seen = defaultdict(lambda: float('inf'))
         while q and steps <= k + 1:
+            
             for i in range(len(q)):
                 curr, price = q.popleft()
-                if (curr in seen and price >= seen[curr]):
-                    continue
+
+                if curr in seen:
+                    if price < seen[curr]:
+                        seen[curr] = price
+                    else:
+                        continue
                 seen[curr] = price
+                if price > cheapest:
+                    continue
                 if curr == dst:
+                    cheapest = min(cheapest, price)
                     continue
                 for node, p in adj[curr]:
                     q.append((node,price+p))
             steps += 1
-        return seen[dst] if seen[dst] != float('inf') else -1
+        return cheapest if cheapest != float('inf') else -1
