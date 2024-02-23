62d1a0248a2cd984a6bbf7a54c148863910eed40
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-23 02:52:09 -0500

    Time: 103 ms (57.44%), Space: 18.2 MB (56.62%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+
+        adj = defaultdict(list)
+
+        for start, end, price in flights:
+            adj[start].append((end, price))
+
+        q = deque([(src, 0)])
+        steps = 0
+        cheapest = float('inf')
+        seen = defaultdict(lambda: float('inf'))
+        while q and steps <= k + 1:
+            for i in range(len(q)):
+                curr, price = q.popleft()
+                if (curr in seen and price >= seen[curr]) or  price > cheapest:
+                    continue
+                seen[curr] = price
+                if curr == dst:
+                    cheapest = min(cheapest, price)
+                    continue
+                for node, p in adj[curr]:
+                    q.append((node,price+p))
+            steps += 1
+        return cheapest if cheapest != float('inf') else -1
