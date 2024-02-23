98fc6bb713e87c75b719d349747455c2524da028
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-23 17:37:06 -0500

    Time: 805 ms (56.87%), Space: 17 MB (77.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,8 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if not amount:
-            return 0
         largest = max(coins)
-        dpCheck = []
-        for n in range(amount+1):
+        dpCheck = [0] # fist case
+        for n in range(1,amount+1):
             minCoin = float('inf')
             for coin in coins:
                 if coin == n:
