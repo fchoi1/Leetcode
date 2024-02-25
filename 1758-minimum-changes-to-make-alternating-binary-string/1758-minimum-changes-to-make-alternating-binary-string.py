e960a950fdbc99231149d22e0ece6100bffaf0e5
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-24 20:28:43 -0500

    Time: 36 ms (96.17%), Space: 16.7 MB (93.99%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -3,12 +3,12 @@ class Solution:
         count1 = count2 = 0
         for i, char in enumerate(s):
             if i % 2 == 0:
-                if char != '0':
+                if char == '0':
                     count1 += 1
                 else:
                     count2 += 1
             else:
-                if char != '1':
+                if char == '1':
                     count1 += 1
                 else:
                     count2 += 1
