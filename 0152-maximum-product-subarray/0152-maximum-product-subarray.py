10da21892c950129739d8a8ebb4b1abe1c400754
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-27 01:51:31 -0500

    Time: 63 ms (94.75%), Space: 17 MB (70.57%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -5,11 +5,17 @@ class Solution:
         firstNeg = 1
         for n in nums:
             product *= n
+
+            # If we hit a negative product that is not the first negative product, check
             if firstNeg < 0 and product < 0:
                 maxP = max(maxP, product//firstNeg)
+
+            # set first negative product we see
             if product < 0 and firstNeg > 0:
                 firstNeg = product
-            maxP = max(product, n, maxP)
+            
+            # check for max
+            maxP = max(product, maxP)
             
             if n == 0:
                 product = 1
