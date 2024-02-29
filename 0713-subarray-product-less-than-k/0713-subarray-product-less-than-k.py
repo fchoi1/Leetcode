43a79a09baa3b69eaef8c3a4a567687631b36b57
Fab <42979283+fchoi1@users.noreply.github.com>
2024-02-29 16:11:26 -0500

    Time: 513 ms (40.83%), Space: 19.4 MB (48.5%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -4,9 +4,8 @@ class Solution:
         product = 1
         for i,n in enumerate(nums):
             product *= n
-            while left < i and product >= k:
+            while left <= i and product >= k:
                 product /= nums[left]
                 left += 1
-            if product < k:
-                count += i - left + 1
+            count += i - left + 1
         return count
