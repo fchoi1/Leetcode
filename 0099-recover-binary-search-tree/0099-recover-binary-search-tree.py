9c04bc3c98022fd51deab01180c120c99218fcd9
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-01 16:41:48 -0500

    Time: 53 ms (86.08%), Space: 16.8 MB (94.31%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
@@ -1,17 +1,5 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        # travense in order
-        # there should be 2 nodes in wrong order (unless at start or end)
-        # check prev 
+    def recoverTree(self, root: Optional[TreeNode]) -> None: 
         self.prev = None
         self.error = None
         self.swapped = False
@@ -20,15 +8,17 @@ class Solution:
                 return
             dfs(node.left)
             # one swap
-            if not self.swapped:
-                if not self.error and self.prev and node.val < self.prev.val:
+            if not self.swapped and self.prev:
+                if not self.error and node.val < self.prev.val:
                     self.error = self.prev
-                if self.error and self.error.val < node.val:
+                elif self.error and self.error.val < node.val:
+                    # swap error node and found prev node
                     self.error.val, self.prev.val = self.prev.val, self.error.val
                     self.swapped = True
             self.prev = node
             dfs(node.right)
         dfs(root)
+        
         if not self.swapped:
             self.prev.val, self.error.val = self.error.val, self.prev.val, 
     
