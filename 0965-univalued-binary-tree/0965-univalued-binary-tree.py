7a35c70554ae7652c43849e448362c91329d2e83
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-01 23:06:47 -0500

    Time: 42 ms (16.07%), Space: 16.6 MB (54.09%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.py
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.py
@@ -1,20 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        self.val = root.val
-        self.unique = True
-        def dfs(node):
-            if not node or not self.unique:
-                return
-            if node.val != self.val:
-                self.unique = False
-                return
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
-        return self.unique
+        if not root or (not root.left and not root.right):
+            return True
+        
+        if root.left and root.val != root.left.val:
+            return False
+
+        if root.right and root.val != root.right.val:
+            return False
+        
+        
+        return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)
