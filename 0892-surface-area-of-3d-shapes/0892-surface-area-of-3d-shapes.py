d660dd44749029c5d1faa615d8a0206b7cb5d27c
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-01 23:23:42 -0500

    Time: 79 ms (49.57%), Space: 16.6 MB (66.24%) - LeetHub

--- 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
+++ 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
@@ -6,7 +6,9 @@ class Solution:
         for y,row in enumerate(grid):
             for x,n in enumerate(row):
                 if n > 0:
+                    # 6 sides subtract 2 sides for each connected cube
                     total += n * 6 - (n-1) * 2
+                # check adjacent and subtract
                 for dx, dy in [[0,1], [1,0]]:
                     if 0 <= x + dx < w and 0 <= y + dy < h:      
                         total -= min(n, grid[dy + y][ dx + x]) * 2
