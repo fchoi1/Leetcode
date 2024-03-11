3405922bfbfa44d2810840c2df26f94b1cbd708c
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-11 10:06:13 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,19 +1,16 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = set()
-        seen = set()
-        N = len(nums)
-
-        def dfs(arr):
-            if len(arr) == N:
+        def dfs(arr, seen):
+            if len(arr) == len(nums):
                 res.add(tuple([i for i in arr]))
             
             for i,val in enumerate(nums):
                 if i in seen:
                     continue
                 seen.add(i)
-                dfs(arr + [val])
+                dfs(arr + [val], seen)
                 seen.remove(i)
-        dfs([])
+        dfs([],0, set())
         return res
         
