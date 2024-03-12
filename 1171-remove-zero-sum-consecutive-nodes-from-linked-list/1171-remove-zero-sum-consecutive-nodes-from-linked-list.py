0a391ba4b1fb43a2282b3c68dbc5edf81e56fe16
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-12 13:02:42 -0400

    Time: 55 ms (19.52%), Space: 16.9 MB (29.48%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -1,41 +1,31 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
-        node = head
-        while node:
-            arr.append(node.val)
-            node = node.next
-        
-        seen = {}
         ignore = set()
         runningSum = 0
-        for i, val in enumerate(arr):
-            runningSum += val
-            if runningSum == 0:
-                ignore.update(range(i+1))
-            else:
-                if runningSum in seen:
-                    if seen[runningSum] + 1 not in ignore:
-                        ignore.update(range(seen[runningSum]+1, i+1))
-            seen[runningSum] = i
-        print(ignore)
-        node = newhead = ListNode()
-        for i, val in enumerate(arr):
-            if i in ignore:
-                continue
-            node.next = ListNode(val)
+        node = head
+        seen = {0:node}
+        while node:
+            runningSum += node.val
+            if runningSum in seen:
+                if runningSum == 0:
+                    ignore.add(seen[runningSum])
+                if runningSum == 0 or seen[runningSum].next not in ignore:
+                    while seen[runningSum] != node:
+                        seen[runningSum] = seen[runningSum].next
+                        ignore.add(seen[runningSum])
+            seen[runningSum] = node
             node = node.next
+
+        newnode = newhead = ListNode()
+        node = head
+        i = 0
+        while node:
+            if node in ignore:
+                node = node.next
+                continue
+            newnode.next = node
+            node = node.next
+            newnode = newnode.next
+        if newnode:
+            newnode.next = None
         return newhead.next
-# 2 1 1 1 -4 3 1
-# 2 3 4 5. 1 4 5
-
-# 1 3 2 -3 -2 5 5 -5 1
-# 1 4 6 3  1  6 11 6 1
-
-# 1 2 -3 3 1
-# 1 3 0 3 4
