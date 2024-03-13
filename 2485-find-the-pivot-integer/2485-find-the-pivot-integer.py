4a7cfe037a0f7afc128ed5ff8db2cc3282f22a58
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-12 20:27:41 -0400

    Time: 34 ms (93.23%), Space: 16.6 MB (23.99%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,12 +1,20 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        for i in range(n,-1, -1):
-            left =(n * (n + 1) / 2 ) - (i * (i + 1) / 2)
-            right = (i-1) * i / 2
+        if n == 1:
+            return 1
+        l = 0
+        r = n
+        # 1 2 3 4 5 6 7 8
+        while l < r:
+            i = (l + r) // 2
+            right = (n * (n + 1) - i * (i + 1))/2
+            left = (i-1) * i / 2
             if left == right:
                 return i
             if left > right:
-                return -1
-
+                r = i - 1
+            else:
+                l = i + 1
+        return -1
 
         
