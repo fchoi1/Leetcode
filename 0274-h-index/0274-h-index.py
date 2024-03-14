e9f348923fdc9c4dc452b809d60fce6a7f6dab37
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-14 00:34:37 -0400

    Time: 44 ms (38.62%), Space: 16.9 MB (33.5%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,7 +1,5 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        if len(citations) == 1:
-            return int(citations[0] >= 1)
         citations.sort()
         h = 0
         for i, c in enumerate(citations):
