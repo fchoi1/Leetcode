7dce428aead27cffea47e88ac3e8d0b395428932
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-16 15:53:30 -0400

    Time: 34 ms (96.28%), Space: 16.6 MB (55.13%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        prev = nums[-1]
+        for i, val in enumerate(nums[::-1]):
+            if prev > val:
+                break
+            prev = val
+        else:
+            nums.sort()
+            return
+
+        # First index to swap
+        index = len(nums)-i-1
+
+        # loop again and get next highests to swap within range
+        for i in range(len(nums)-1, index, -1):
+            if nums[i] > nums[index]:
+                break
+
+        # swap indexes
+        nums[index], nums[i] = nums[i], nums[index]
+        # reverse the sub array (no need to sort since it is already in descending order)
+        nums[index + 1:] = nums[index + 1:][::-1]
+        return
