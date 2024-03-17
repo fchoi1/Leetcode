5ed0608ac229a891567e196c2b08b14387917e03
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-16 21:54:59 -0400

    Time: 74 ms (65.65%), Space: 20 MB (39.6%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,41 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-
         if not intervals:
             return [newInterval]
-        
-        minStart = newInterval[0]
-        maxEnd = newInterval[1]
-        if minStart > intervals[-1][1]:
-            return intervals + [newInterval] 
-        if maxEnd < intervals[0][0]:
-            return [newInterval] + intervals
-
-        firstInt = None
-        prev = None
-        ignore = set()
-        for (start, end) in intervals:
-            # Overlaps
-            if (end >= minStart and end <= maxEnd) or (start >= minStart and start <= maxEnd) or (newInterval[0] >= start and newInterval[1] <= end):
-                if not firstInt:
-                    firstInt = (start, end)
-                minStart = min(minStart, start)
-                maxEnd = max(maxEnd, end)
-                ignore.add((start, end))
-            # No Overlaps
-            if prev and newInterval[0] > prev[1] and newInterval[1] < start and not firstInt:
-                firstInt = (start, end)
-                break
-            if start > maxEnd:
-                break
-            prev = (start, end)
         res = []
-        for (start, end) in intervals:
-            if (start, end) == firstInt:
-                res.append((minStart, maxEnd))
-
-            if (start, end) in ignore:
+        newStart = newInterval[0]
+        newEnd = newInterval[1]
+        for start, end in intervals:
+            if end < newInterval[0] or start > newInterval[1]:
+                if start > newInterval[1] and newStart is not None:
+                    res.append([newStart, newEnd])
+                    newStart = None
+                res.append([start,end])
                 continue
-            
-            res.append((start, end))
-        return res        
+            newStart = min(newStart, start)
+            newEnd = max(newEnd, end)
+        print(res)
+        if newStart is not None:
+            res.append([newStart, newEnd])
+        return res
+        
