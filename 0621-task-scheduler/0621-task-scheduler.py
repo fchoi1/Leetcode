d7b861e3c543acb384de71618a532a14c456d95b
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-19 01:21:51 -0400

    Time: 400 ms (72.48%), Space: 17 MB (79.06%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,28 +1,24 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # start with highest freq
         count = Counter(tasks)
         heap = [-freq for val, freq in count.items()]
-        print(heap)
         cycles = 0
         while heap:
             temp = []
             for i in range(n+1):
-                if heap:
-                    freq = heapq.heappop(heap)
-                    freq += 1
-                    if freq == 0:
-                        if not heap and not temp:
-                            cycles += i + 1
-                            return cycles
-                        continue
+                if not heap:
+                    break
+                freq = heapq.heappop(heap)
+                freq += 1
+                if freq == 0 and not heap and not temp:
+                    return cycles + i + 1
+                elif freq != 0:
                     temp.append(freq)
             cycles += n+1
             for f in temp:
                 heapq.heappush(heap, f)
             
-        return cycles
+        return -1
 
-        # wait full cycle, then pop back things into heap?
 
         
