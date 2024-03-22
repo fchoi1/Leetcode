e8e7a280c59ab76f1e8287fcf4dc9ecac0a405ff
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-21 21:32:41 -0400

    Time: 281 ms (76.71%), Space: 32 MB (70.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,12 +1,5 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-
-        # reverse
 
         slow = head
         fast = head
