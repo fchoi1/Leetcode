a52ddf3d4180349a46fe3ce11414b38f79a57523
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-25 17:53:46 -0400

    Time: 63 ms (51.36%), Space: 17.3 MB (79.05%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -1,8 +1,7 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
 
-        i = 0
-        zeros = 0
+        i = zeros = 0
         q = deque([])
         while i < len(arr):
             q.append(arr[i])
@@ -10,15 +9,8 @@ class Solution:
             if zeros > 0:
                 arr[i] = 0
                 zeros -= 1
-                i += 1
-                continue
-            
-            if q[0] == 0:
-                zeros += 1
-            
-            arr[i] = q.popleft()
+            else:
+                if q[0] == 0:
+                    zeros += 1
+                arr[i] = q.popleft()
             i += 1
- 
-
-        # 1 0 0 3 4
-        # 1 0 0 0 0
