f8b4f01f2e14754965ffc47f4bcc211a2f835b69
Fab <42979283+fchoi1@users.noreply.github.com>
2024-03-28 21:15:21 -0400

    Time: 65 ms (16.28%), Space: 18.2 MB (5.35%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -5,8 +5,7 @@ class Solution:
             if loop:
                 return
             if node in seen:
-                if path[0] == path[-1]:
-                    loop = path.copy()
+                loop = path.copy()
                 return
             seen.add(node)
             for n in adjMap[node]:
@@ -17,17 +16,29 @@ class Solution:
                 path.pop()
             seen.remove(node)
 
+        # Adj Map
         adjMap = defaultdict(set)
         for a,b in edges:
             adjMap[a].add(b)
             adjMap[b].add(a)
         
+        # Find loop path
         loop = None
         for i in range(len(edges)):
             if loop:
                 break
             dfs(i+1, [i+1], set(), None)
-        # print(loop)
+
+        # find acutally loop
+        seen = {}
+        
+        for i, n in enumerate(loop):
+            if n in seen:
+                loop = loop[seen[n]:i]
+                break
+            seen[n] = i
+
+        print(loop)
         edgesSet = set()
         for a,b in zip(loop, loop[1:]):
             edgesSet.add((min(a,b), max(a,b)))
