0f1b199374077b48c891188370551bd92463f0af
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-02 20:53:01 -0400

    Time: 6786 ms (10.74%), Space: 16.5 MB (71.59%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        W,H = len(board[0]), len(board)
+
+        def inRange(x,y):
+            return 0 <= x < W and 0 <= y < H
+        
+        def traverse(x,y, index, seen):
+            if index >= len(word) or word[index] != board[y][x]:
+                return False
+            if index == len(word) - 1:
+                return True
+            
+            for dx, dy in [[0,1],[1,0],[-1,0],[0,-1]]:
+                if inRange(x+dx, y+dy) and (x+dx, y+dy) not in seen:
+                    seen.add((x,y))
+                    if traverse(x+dx, y+dy, index + 1, seen):
+                        return True
+                    seen.remove((x,y))
+            return False
+            
+        for y, row in enumerate(board):
+            for x, row in enumerate(row):
+                if traverse(x,y,0,set()):
+                    return True
+        return False
+
+        
