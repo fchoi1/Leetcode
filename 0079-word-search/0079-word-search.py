40726b0ee8ddf469be0e8a45945e0df5a381521e
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-02 20:56:45 -0400

    Time: 6531 ms (11.92%), Space: 16.5 MB (95.32%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -6,22 +6,22 @@ class Solution:
             return 0 <= x < W and 0 <= y < H
         
         def traverse(x,y, index, seen):
-            if index >= len(word) or word[index] != board[y][x]:
+            if index >= len(word) or word[index] != board[y][x] or (x,y) in seen:
                 return False
             if index == len(word) - 1:
                 return True
             seen.add((x,y))
             for dx, dy in [[0,1],[1,0],[-1,0],[0,-1]]:
-                if inRange(x+dx, y+dy) and (x+dx, y+dy) not in seen:
-                    if traverse(x+dx, y+dy, index + 1, seen):
+                if inRange(x+dx, y+dy) and traverse(x+dx, y+dy, index + 1, seen):
                         return True
             seen.remove((x,y))
             return False
             
         for y, row in enumerate(board):
             for x, row in enumerate(row):
-                if traverse(x,y,0,set()):
-                    return True
+                if board[y][x] == word[0]:
+                    if traverse(x,y,0,set()):
+                        return True
         return False
 
         
