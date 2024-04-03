215d824832f882be09c99780b1d6e696befc3753
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-03 15:47:25 -0400

    Time: 43 ms (53.36%), Space: 16.5 MB (63.64%) - LeetHub

--- 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.py
+++ 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.py
@@ -1,11 +1,12 @@
 class Solution:
     def findTheLongestBalancedSubstring(self, s: str) -> int:
         zeros = longest = ones = 0
+        prev = None
 
-        for i, val in enumerate(s):
+        for val in s:
             if val == '0':
                 # Reset counts if a zero appears after 1
-                if i > 0 and s[i-1] == '1':
+                if prev == '1':
                     zeros = 0
                     ones = 0
                 zeros += 1
@@ -13,6 +14,7 @@ class Solution:
                 ones += 1
             if ones <= zeros:
                 longest = max(longest,  ones * 2)
+            prev = val
 
         return longest
         
