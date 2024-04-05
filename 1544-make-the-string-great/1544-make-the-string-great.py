76e08dd44f2a4671783f9809bc3eecf9551f7abb
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-04 21:09:21 -0400

    Time: 42 ms (27.78%), Space: 16.5 MB (91.04%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,13 +1,10 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        # two pointer
         string = []
         for char in s:
-            # bad
             if string and char != string[-1] and char.lower() == string[-1].lower():
                 string.pop()
                 continue
             else:
                 string.append(char)
-        
         return "".join(string)
