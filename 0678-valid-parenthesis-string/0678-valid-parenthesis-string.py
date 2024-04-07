305d301c788f2fb2e6e94e4e5720943ea80b7ea1
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-06 22:01:51 -0400

    Time: 33 ms (74.42%), Space: 16.4 MB (91.06%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        count = 0
+        o_bracket = 0 # checking for when closed > open
+        c_bracket = 0 # checking after of when closed == open
+        for char in s:
+            if char == "(":
+                c_bracket += 1
+                o_bracket += 1
+            elif char == ")":
+                o_bracket -= 1
+                c_bracket = max(0, c_bracket - 1)
+            else:
+                o_bracket += 1
+                c_bracket = max(0, c_bracket - 1)
+                
+            if o_bracket < 0:
+                return False
+        return c_bracket == 0
+        
