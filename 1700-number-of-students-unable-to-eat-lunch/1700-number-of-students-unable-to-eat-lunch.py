8113b3f8231f5dac8668d223bcb431968f293664
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-07 21:10:28 -0400

    Time: 43 ms (35.74%), Space: 16.6 MB (59.9%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,11 +1,8 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        # 0 1
-        # 0 1
         ones = students.count(1)
         zeros = students.count(0)
         while ones > 0 and zeros > 0 and sandwiches and students:
-            print("here",ones, zeros, students, sandwiches)
             while students and sandwiches and students[0] == sandwiches[0]:
                 if students[0]:
                     ones -= 1
@@ -16,14 +13,4 @@ class Solution:
             if not sandwiches or not students:
                 break
             students.append(students.pop(0))
-        print("done", ones , zeros , sandwiches)
         return len(sandwiches)
-        # 1  1 1 0 0 1
-        # 1  0 0 0 1 1
-
-        # 1 1 1
-        # 0 1 1
-
-        # 0 0 0 0
-        # 1 0 0 0
-        
