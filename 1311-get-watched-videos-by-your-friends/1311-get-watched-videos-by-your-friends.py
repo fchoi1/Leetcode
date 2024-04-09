42586cb0e3064a3fbbaf7a90002e319c9a28adf6
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-09 14:00:37 -0400

    Time: 248 ms (43.33%), Space: 18.6 MB (35.83%) - LeetHub

--- 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.py
+++ 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.py
@@ -1,10 +1,6 @@
 class Solution:
     def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
-
-        # adj grid
         friendMap = defaultdict(set)
-
-        # get k level friends
         seen = set([id])
         q = deque([id])
         for _ in range(level):
@@ -14,12 +10,9 @@ class Solution:
                     if f not in seen:
                         seen.add(f)
                         q.append(f)
-        print(q)
         counts = defaultdict(int)
         for friend in q:
             for vids in watchedVideos[friend]:
                 counts[vids] += 1
-        # items = sorted(counts.items(), key=lambda x: (x[1], x[0]))
-        # print(items)
         return [x[0] for x in sorted(counts.items(), key=lambda x: (x[1], x[0]))]
 
