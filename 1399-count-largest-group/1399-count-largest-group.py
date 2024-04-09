35a475ccb8faf2b33804f0d16ac3a50d07eb724a
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-09 15:53:07 -0400

    Time: 96 ms (34.16%), Space: 16.4 MB (85.4%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.py
+++ 1399-count-largest-group/1399-count-largest-group.py
@@ -6,6 +6,4 @@ class Solution:
         for i in range(1, n+1):
             s = sum(int(x) for x in str(i))
             arr[s-1] += 1
-        
-        m = max(arr)
-        return sum(x == m for x in arr)
+        return sum(x ==  max(arr) for x in arr)
