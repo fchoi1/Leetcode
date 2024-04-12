97c7c3880090775552c31513f49ff37100fd5856
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-12 11:40:38 -0400

    Time: 262 ms (18.6%), Space: 23.9 MB (88.02%) - LeetHub

---
+++ 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxAbsValExpr(self, arr1: List[int], arr2: List[int]) -> int:
+        
+
+
+        # (x1 - y1) + (x2 - y2) = i - j 
+        # (y1 - x1) + (x2 - y2) = i - j
+        # (y1 - x1) + (y2 - x2) = i - j
+        # (x1 - y1) + (y2 - x2) = i - j
+
+        # f(i) = a(x_i) +b(y_i) for a = [1,-1,1,-1] and b = [1,1,-1,-1]
+        # f(i) - f(j) -> get this maxVal
+        maxVal = 0
+        for a,b in [[1,1],[-1,1],[1,-1],[-1,-1]]:
+            smallest = a * arr1[0] + b * arr2[0] 
+            for i,(x,y) in enumerate(zip(arr1, arr2)):
+                curr = a * x + b * y + i
+                maxVal = max(maxVal, curr-smallest)
+                smallest = min(smallest, curr)
+        return maxVal
+
+
