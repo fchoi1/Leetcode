771a101896cd89c32da46833a642ef99edaf1143
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-11 20:22:18 -0400

    Time: 108 ms (39.25%), Space: 18.5 MB (39.1%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,14 +1,13 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # monotonic stacK?
-        left = [height[0]]
-        right = [height[-1]]
         N = len(height)
+        left = [0] * N
+        right = [0] * N
+        left[0],right[-1] = height[0],height[-1]
         for i in range(1,N):
-            left.append(max(left[-1], height[i]))
-            right.append(max(right[-1], height[-i]))
-        right.reverse()
-        
+            left[i] = max(left[i-1], height[i])
+            right[-i-1] = max(right[N-i], height[N-i])
+
         water = 0
         for l,r, h in zip(left, right, height):
             if h < l and h < r:
