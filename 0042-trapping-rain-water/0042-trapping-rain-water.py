70bd6de3fac174a669de2f01783250bff4839176
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-11 20:21:52 -0400

    Time: 116 ms (21.75%), Space: 18.5 MB (72.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -8,11 +8,10 @@ class Solution:
             left.append(max(left[-1], height[i]))
             right.append(max(right[-1], height[-i]))
         right.reverse()
-        # print(left, right)
+        
         water = 0
         for l,r, h in zip(left, right, height):
             if h < l and h < r:
-                print(l,h,r)
                 water += min(l,r) - h
 
         return water
