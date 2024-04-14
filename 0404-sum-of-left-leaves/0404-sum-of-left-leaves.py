837586318c985ac67b49898a6da63d877b7e3b39
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-13 21:41:49 -0400

    Time: 42 ms (16.81%), Space: 16.8 MB (47.79%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -1,17 +1,17 @@
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        self.sum = 0
+        self.leftSum = 0
+
         def traverse(node, isLeft):
             if not node:
-                return 
-
-            if not node.left and not node.right and isLeft:
-                self.sum += node.val
+                return
+            
+            if isLeft and not node.left and not node.right:
+                self.leftSum += node.val
                 return
             
             traverse(node.left, True)
             traverse(node.right, False)
-            
         traverse(root, False)
-        return self.sum
-        
+
+        return self.leftSum
