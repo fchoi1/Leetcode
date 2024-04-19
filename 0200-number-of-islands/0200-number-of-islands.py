d5225880ce869389041bb05a5eddd7b6754d2078
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-18 20:11:50 -0400

    Time: 225 ms (90.96%), Space: 18.9 MB (61.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-  
-      visited = set()
-      m = len(grid)
-      n = len(grid[0])
-      
-      def inRange(x,y):
-          return 0 <= x < m and 0 <= y < n
+        W = len(grid[0])
+        H = len(grid)
 
-      def foundIsland(row, col, grid):
-        directions = [(row-1, col), (row, col-1), (row+1,col), (row,col+1)]
-        for (x, y) in directions:
-          if inRange(x,y) and grid[x][y] == "1" and (x,y) not in visited:
-                visited.add((x,y))
-                foundIsland(x, y, grid)
-        
-      count = 0
-      for row in range(m):
-        for col in range(n):
-            if grid[row][col] == "1" and (row,col) not in visited:
-                foundIsland(row, col, grid)
-                count += 1
-      return count
+        def traverse(x,y):
+            if x < 0 or x >= W or y < 0 or y >= H:
+                return
+            if grid[y][x] == '0':
+                return
+            grid[y][x] = '0'
+            for dx,dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+                traverse(x+dx, y+dy)
+
+        islands = 0
+        for y in range(H):
+            for x in range(W):
+                if grid[y][x] == '1':
+                    islands += 1
+                    traverse(x,y)
+                    
+        return islands
+                
