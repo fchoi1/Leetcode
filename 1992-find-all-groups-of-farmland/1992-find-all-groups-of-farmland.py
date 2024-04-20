2a85b1971ade7bc5405fdb216f7581a0a61c67a9
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-19 20:54:08 -0400

    Time: 1230 ms (5%), Space: 43.6 MB (14%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        farmland = []
+
+        H = len(land)
+        W = len(land[0])
+
+        def traverse(x,y,botX, botY):
+            if x < 0 or x >= W or y < 0 or y >= H or not land[y][x]:
+                return (botX, botY)
+
+            land[y][x] = 0 
+            botX, botY = max(botX, x),max(botY, y)
+            print(x,y, "bot", botX, botY)
+
+            for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
+                botX, botY = traverse(x+dx,y+dy, botX, botY)
+            return (botX, botY)
+
+
+        for y in range(H):
+            for x in range(W):
+                if land[y][x]:
+                    botX, botY = traverse(x,y,x,y)
+                    farmland.append([y,x,botY,botX])
+
+        return farmland
+        
