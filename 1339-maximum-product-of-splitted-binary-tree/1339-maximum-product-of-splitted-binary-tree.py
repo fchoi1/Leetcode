8031e9c9965acc012797989bb7bfe6fc97a8e16a
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-25 23:09:49 -0400

    Time: 204 ms (10.33%), Space: 37.8 MB (15.67%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -2,13 +2,7 @@
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
         self.total = 0
-        def getTotal(node):
-            if not node:
-                return
-            self.total += node.val
-            getTotal(node.left)
-            getTotal(node.right)
-        getTotal(root)
+        self.product = 1
 
         def getSum(node, currVal):
             if not node:
@@ -18,7 +12,8 @@ class Solution:
             right = getSum(node.right, 0)
             self.product = max(self.product, left *(self.total - left), right *(self.total - right))
             return left + right + currVal
-        
+            
+        self.total = getSum(root,0)
         self.product = 1
         getSum(root,0)
         return self.product  % (10 ** 9 + 7)
