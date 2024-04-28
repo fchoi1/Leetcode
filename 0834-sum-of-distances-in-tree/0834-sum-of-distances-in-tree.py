13ef0c8ee1f583329d987588b88e6e370ac12e98
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-27 23:12:52 -0400

    Time: 900 ms (11.65%), Space: 48.4 MB (10.84%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
@@ -1,14 +1,5 @@
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
-        # 0 1 2 3 3 3 
-        # 1 0 1 2 2 2  
-        # 2 1 0 1 1 1
-        # 3 2 1 0 2 2
-        # 3 2 1 2 0 2
-        
-
-        #。+ 1 for all next ones
-        # -1 for all prev ones
         adjMap = defaultdict(set)
         for a, b in edges:
             adjMap[a].add(b)
@@ -26,25 +17,15 @@ class Solution:
                 nodeDist, count = getDist(nextNode)
                 totalDist += nodeDist
                 nodeCount += count
-                # memomize
-            print(node, totalDist + nodeCount, nodeCount)
-            memo[node] = (totalDist + nodeCount, nodeCount + 1)
+            memo[node] = nodeCount + 1 
             return totalDist + nodeCount, nodeCount + 1
 
         ans[0] = getDist(0)[0]
-        print(memo)
-        # 2: 8 - 4 -3 +  2nodes, + 3 = 6
-        # 1: 8  -0 -1 +  5nodes, + 0
-
-        # 3: 6 - 1 + 5, +0
-
         seen = set()
         def traverse(node, prev):
             seen.add(node)
-            print("checking", node, prev)
             if prev != None:
-                ans[node] = ans[prev] - memo[node][1] + n - memo[node][1] 
-                print(node, ans[node], ans[prev],memo[node])
+                ans[node] = ans[prev] + n - memo[node] * 2
             for nextNode in adjMap[node]:
                 if nextNode in seen:
                     continue
