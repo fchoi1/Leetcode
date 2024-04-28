4a291d7df1a211a63c2fa5b17866c62ec5a07a4d
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-27 23:31:48 -0400

    Time: 34 ms (95.47%), Space: 16.7 MB (49.97%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        # memo
+        string = "1"
+
+        for _ in range(n-1):
+            new = ""
+            count = 1
+            prev = string[0]
+            for c in string[1:]:
+                if prev == c:
+                    count += 1
+                else:
+                    new += str(count) + prev
+                    count = 1
+                prev = c
+            new  += str(count) + prev
+            string = new
+        return string
