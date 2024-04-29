00ea8f51d051e05a32c7175ccb8fe484ac0555e8
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-28 20:29:58 -0400

    Time: 595 ms (32.48%), Space: 31 MB (50.32%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -6,25 +6,4 @@ class Solution:
         
         kStr = (bin(k)[2:]).zfill(20)
         currStr = (bin(curr)[2:]).zfill(20)
-        print(curr,kStr,currStr)
-
-        steps = 0
-        for a,b in zip(kStr, currStr):
-            if a != b:
-                steps += 1
-        return steps
-        # 2 1 3 4
-
-        # 010
-        # 001
-        #----
-        # 011
-
-        # 011
-        #----
-        # 000
-        # 100
-        # ---
-        # 100
-        
-        # 001
+        return sum(a!=b for a,b in zip(kStr, currStr))
