19ddda9bdd18d11e00d18e3a746a621edc3a53ab
Fab <42979283+fchoi1@users.noreply.github.com>
2024-04-29 21:06:08 -0400

    Time: 2112 ms (8.66%), Space: 17.8 MB (8.65%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        seen = defaultdict(int)
+        val = 0
+        wonderful = 0
+        seen[val] += 1
+        for char in word:
+            print("char", char)
+            val ^= 1 << (ord(char)-ord('a'))
+            
+            wonderful += seen[val]
+            for n in range(10):
+                oneOff = val ^ (1 << n)
+                if oneOff in seen:
+                    wonderful += seen[oneOff] 
+            seen[val] += 1
+        return wonderful
