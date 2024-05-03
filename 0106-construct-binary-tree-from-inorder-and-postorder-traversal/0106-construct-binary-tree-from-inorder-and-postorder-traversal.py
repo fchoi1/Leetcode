8dd6d604a29ccc44675765f8692612537fd1c034
Fab <42979283+fchoi1@users.noreply.github.com>
2024-05-03 02:02:53 -0400

    Time: 190 ms (6.79%), Space: 88.3 MB (23.35%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,21 @@
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+
+        def build(in_order, post_order):
+            if not post_order or not in_order:
+                return None
+
+            parentVal = post_order[-1]
+            i = 0
+            for val in in_order:
+                if val == parentVal:
+                    break
+                i += 1
+
+            parent = TreeNode(parentVal)
+            parent.left = build(in_order[:i], post_order[:i])
+            parent.right = build(in_order[i+1:], post_order[i:-1])
+            
+            return parent
+
+        return build(inorder, postorder)
