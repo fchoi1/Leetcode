7c420cb90bd56aaef8469cc65f129f0bba638847
Fab <42979283+fchoi1@users.noreply.github.com>
2024-05-03 02:06:00 -0400

    Time: 207 ms (5.61%), Space: 88.4 MB (12.77%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -5,7 +5,7 @@ class Solution:
             if not post_order or not in_order:
                 return None
 
-            parentVal = post_order[-1]
+            parentVal = post_order.pop()
             i = 0
             for val in in_order:
                 if val == parentVal:
@@ -14,8 +14,7 @@ class Solution:
 
             parent = TreeNode(parentVal)
             parent.left = build(in_order[:i], post_order[:i])
-            parent.right = build(in_order[i+1:], post_order[i:-1])
-            
+            parent.right = build(in_order[i+1:], post_order[i:])
             return parent
 
         return build(inorder, postorder)
