ee785647e9121fdd25eb2bbe73d55e66052e2e4c
Fab <42979283+fchoi1@users.noreply.github.com>
2024-05-04 22:45:26 -0400

    Time: 917 ms (33.94%), Space: 35.5 MB (21.85%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -3,26 +3,26 @@ class Solution:
         if len(nums) < 2:
             return 0
 
-        minVal ,maxVal = min(nums), max(nums) 
+        minVal , maxVal = min(nums), max(nums) 
         if maxVal - minVal == 0:
             return 0
         intervals = (maxVal - minVal) / (len(nums) - 1)
-        buckets = [(inf, -inf) for _ in range(len(nums))]
+        buckets = [[inf, -inf] for _ in range(len(nums))]
 
         for n in nums:
             index = int((n-minVal) / intervals)
-            buckets[index] = (min(n,buckets[index][0]), max(n, buckets[index][1]))
+            if n < buckets[index][0]:
+                buckets[index][0] = n
+            if n > buckets[index][1]:
+                buckets[index][1] = n
 
         maxDiff = 0
         prev = buckets[0][0]
         for a,b in buckets:
             if a == inf:
                 continue
-            maxDiff = max(maxDiff, a-prev)
+            if a-prev > maxDiff:
+                maxDiff = a-prev
             prev = b
         return maxDiff 
         
-
-        # 1,2, 5, 6, 8
-
-        # ans = 5,2
