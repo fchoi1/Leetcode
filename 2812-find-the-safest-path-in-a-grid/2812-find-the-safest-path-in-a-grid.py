a245e347525a52b9d37f5de88934cf6df16a3955
Fab <42979283+fchoi1@users.noreply.github.com>
2024-05-14 23:46:54 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
-        self.W, self.H = len(grid[0]), len(grid)
-        self.safe = [[inf for _ in range(self.W)] for _ in range(self.H)]   
+        N =  len(grid)
+        self.safe = [[inf for _ in range(N)] for _ in range(N)]   
     
         def traverse(x ,y):
             q = deque([(x,y)])
@@ -10,42 +10,38 @@ class Solution:
             while q:
                 for _ in range(len(q)):
                     x,y = q.popleft()
-
                     if (x,y) in seen or step >= self.safe[y][x]:
                         continue
                     seen.add((x,y))
                     self.safe[y][x] = step
                                         
                     for dx, dy in [(0,1), (1,0), (-1,0), (0, -1)]:
-                        if 0 <= x + dx < self.W and 0 <= y + dy < self.H and grid[y + dy][x + dx] != 1:
+                        if 0 <= x + dx < N and 0 <= y + dy < N and grid[y + dy][x + dx] != 1 and (x + dx, y + dy) not in seen:
                             q.append((x + dx, y + dy))
                 step += 1
 
-        
         seen = set()
        
-
-        for y in range(self.H):
-            for x in range(self.W):
+        for y in range(N):
+            for x in range(N):
                 if grid[y][x]:
                     traverse(x, y)
     
         # dfs
         q = [[-self.safe[0][0],0,0]]
 
-        if grid[self.H-1][self.W-1] == 1:
-            return 0
-
         while q:
             highest, x, y = heapq.heappop(q)
             curr = min(-highest, self.safe[y][x])
-            if (x,y) == (self.W-1, self.H-1):
+            if (x,y) == (N-1, N-1):
                 return curr
+
             if (x,y) in seen:
+                print("hih", x,y)
                 continue
             seen.add((x,y))         
             for dx, dy in [(0,1), (1,0), (-1,0), (0, -1)]:
-                if 0 <= x + dx < self.W and 0 <= y + dy < self.H and grid[y + dy][x + dx] != 1 and (x + dx, y + dy) not in seen:
-                    heapq.heappush(q,(-curr, dx + x, dy + y))
+                if 0 <= x + dx < N and 0 <= y + dy < N and grid[y + dy][x + dx] != 1:
+                    heapq.heappush(q,[-curr, dx + x, dy + y])
         return 0
     
