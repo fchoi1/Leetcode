72fdd1247822595f6981b614f3a19e3c76ec2a10
Fab <42979283+fchoi1@users.noreply.github.com>
2024-05-16 20:16:47 -0400

    Time: 50 ms (24.84%), Space: 17 MB (80.39%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
@@ -7,14 +7,11 @@ class Solution:
             traverse(node.left, node)
             traverse(node.right, node)
             
-            if not node.left and not node.right and parent:
-                if node.val == target:
+            if not node.left and not node.right and parent and node.val == target:
                     if node == parent.left:
                         parent.left = None
                     else:
                         parent.right = None
-                    return True
-                return False
             
         traverse(root,None)
         
