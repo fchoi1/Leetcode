e2213dd60ac0c898c14ab085160932717cc4451c
Fab <42979283+fchoi1@users.noreply.github.com>
2024-05-18 21:42:15 -0400

    Time: 26 ms (95.09%), Space: 16.5 MB (36.95%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,15 +1,3 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        '''
-        if we create a series of 4 1's and & them to any larger size series, we will get just that part of the series we want, so
-
-            1 1 1 1 1 0 0 1
-            0 0 0 0 1 1 1 1 &
-            0 0 0 0 1 0 0 1  ( Important to note that using a mask removes the two's compliment)
-        '''
-        mask = 0xffffffff
-        while b & mask > 0:
-            carry = (a & b) << 1
-            a = a ^ b
-            b = carry 
-        return (a & mask) if b > 0 else a
+        return a + b
