29bf5c265b7c048a5720053e2b3044ae967238d6
Fab <42979283+fchoi1@users.noreply.github.com>
2024-05-20 23:56:45 -0400

    Time: 35 ms (72.93%), Space: 16.6 MB (77.55%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        combos = []
-        def backtrack(index, array):
-            combos.append(array[:])
-            if index == len(nums):
+        unique = []
+        def backtrack(index, currArray):
+            unique.append(currArray[:])
+            if index >= len(nums):
                 return
+            
             for i,n in enumerate(nums[index:]):
-                array.append(n)
-                backtrack(i + index + 1, array)
-                array.pop()
+                currArray.append(n)
+                backtrack(index + 1 + i, currArray)
+                currArray.pop()
+                
         backtrack(0, [])
-        
-        return combos
+        return unique
+
         
