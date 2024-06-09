33c81c175d88b0e7fda639cbfb5bf58c16d1364e
Fab <42979283+fchoi1@users.noreply.github.com>
2024-06-08 21:29:26 -0400

    Time: 125 ms (7.14%), Space: 58.9 MB (14.29%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
@@ -5,14 +5,12 @@
  */
 var subarraysDivByK = function(nums, k) {
     const counts = {0: 1}
-    let currSum = 0
-    let ans = 0
+    let currSum = 0, ans = 0
     nums.forEach(val => {
         currSum = (currSum + val) % k
         if (currSum < 0) currSum += k
-        if (currSum in counts){
-            ans += counts[currSum]
-        }else counts[currSum] = 0
+        if (currSum in counts) ans += counts[currSum]
+        else counts[currSum] = 0
         counts[currSum] ++
     })
     console.log(counts)
