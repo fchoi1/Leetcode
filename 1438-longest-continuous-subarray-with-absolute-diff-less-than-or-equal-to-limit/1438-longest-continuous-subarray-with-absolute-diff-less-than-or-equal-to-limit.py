600a5ef52eb0e7d1bc0ee30da88e42aa56650572
Fab <42979283+fchoi1@users.noreply.github.com>
2024-06-22 21:07:33 -0400

    Time: 619 ms (22%), Space: 42.1 MB (5.24%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,6 +1,5 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        # sliding windows and heap?
         maxHeap = []
         minHeap = []
         removed = set()
@@ -10,32 +9,26 @@ class Solution:
         for i,n in enumerate(nums):
             heapq.heappush(maxHeap, (-n, i))
             heapq.heappush(minHeap, (n, i))
-            # print(i, "check", n)
             if -maxHeap[0][0] - minHeap[0][0] <= limit:
                 longest = max(longest,i - l)
-                # print(longest,"adding longest", i, l, maxHeap, minHeap)
             else:
-                # print("pop heap")
                 while maxHeap and minHeap and -maxHeap[0][0] - minHeap[0][0] > limit or (maxHeap[0][1] in removed or minHeap[0][1] in removed):
 
-                    if maxHeap[0][1] in removed:
-                        heapq.heappop(maxHeap)
-                        continue
-                    elif minHeap[0][1] in removed:
-                        heapq.heappop(minHeap)
-                        continue
+                    # if maxHeap[0][1] in removed:
+                    #     heapq.heappop(maxHeap)
+                    #     continue
+                    # elif minHeap[0][1] in removed:
+                    #     heapq.heappop(minHeap)
+                    #     continue
 
                     if maxHeap[0][1] < minHeap[0][1]:
                         idx = heapq.heappop(maxHeap)[1]
-                        removed.add(idx)
-                        l = max(l,idx+1) 
                     else:
                         idx = heapq.heappop(minHeap)[1]
-                        removed.add(idx)
-                        l = max(l,idx+1) 
+                    removed.add(idx)
+                    l = max(l,idx+1) 
                 
 
-                # print("end",l, maxHeap, minHeap)
         return longest + 1
 
         # 8 8 8 8
