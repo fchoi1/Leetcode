71d5e6503177631c0e9a729c309e3dae28fc7928
Fab <42979283+fchoi1@users.noreply.github.com>
2024-06-22 21:14:33 -0400

    Time: 669 ms (17.65%), Space: 42.2 MB (5.24%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -3,23 +3,16 @@ class Solution:
         maxHeap = []
         minHeap = []
         removed = set()
-        l = 0
-        longest = 0
+        l = longest = 0
 
         for i,n in enumerate(nums):
             heapq.heappush(maxHeap, (-n, i))
             heapq.heappush(minHeap, (n, i))
+
             if -maxHeap[0][0] - minHeap[0][0] <= limit:
                 longest = max(longest,i - l)
             else:
-                while maxHeap and minHeap and -maxHeap[0][0] - minHeap[0][0] > limit or (maxHeap[0][1] in removed or minHeap[0][1] in removed):
-
-                    # if maxHeap[0][1] in removed:
-                    #     heapq.heappop(maxHeap)
-                    #     continue
-                    # elif minHeap[0][1] in removed:
-                    #     heapq.heappop(minHeap)
-                    #     continue
+                while maxHeap and minHeap and -maxHeap[0][0] - minHeap[0][0] > limit:
 
                     if maxHeap[0][1] < minHeap[0][1]:
                         idx = heapq.heappop(maxHeap)[1]
@@ -27,13 +20,5 @@ class Solution:
                         idx = heapq.heappop(minHeap)[1]
                     removed.add(idx)
                     l = max(l,idx+1) 
-                
 
         return longest + 1
-
-        # 8 8 8 8
-        # 8 2 2 2
-
-        # 7 7 7 8
-        # 7 4 2 2
-        
