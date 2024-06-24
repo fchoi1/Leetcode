e4a49d49924ef74089a211d617edd98d75b12e1b
Fab <42979283+fchoi1@users.noreply.github.com>
2024-06-23 21:56:39 -0400

    Time: 9261 ms (5.63%), Space: 19.5 MB (92.96%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -3,8 +3,6 @@ sys.set_int_max_str_digits(0)
 
 class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
-        # 0
-        # xor 
         if k == 1:
             return Counter(nums)[0]
 
@@ -12,17 +10,10 @@ class Solution:
         for i, n in enumerate(reversed(nums[:k])):
             if n:
                 currNum += 2 ** i
-    
         counts = 0
         xor = 2 ** k - 1
         limit = 2 ** (k - 1)
-
         i = k
-
-        # [1,1,0,1,0,1,1,0,0]
-        #  1 1 1 0 0 1 1 0 0
-        # 1 1 1  1 1 1 1 0 0
-        # 1 1 1 1 1 1 1 1 1
         if currNum < limit:
             counts += 1
             currNum ^= xor
@@ -45,5 +36,4 @@ class Solution:
         if currNum == 0:
             counts += 1
             currNum = xor
-        print("Done", currNum, counts)
         return counts if currNum == xor else -1
