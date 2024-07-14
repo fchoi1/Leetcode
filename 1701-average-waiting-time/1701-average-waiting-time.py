3e85c12fea0524ced94ec50240535ebd06932989
Fab <42979283+fchoi1@users.noreply.github.com>
2024-07-13 21:19:40 -0400

    Time: 805 ms (6.85%), Space: 57.6 MB (15.68%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -0,0 +1,22 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        q = []
+        N = len(customers)
+        total = 0
+        curr = 0
+        for a,t in customers:
+            if q:
+                q.append((a, max(q[-1][1], a) + t))
+   
+            else:
+                q.append((a, a+t))
+        
+        while q:
+            start, end = q.pop()
+            total += (end - start)
+        print("total", total)
+        return total / N
+
+        
+
+        
