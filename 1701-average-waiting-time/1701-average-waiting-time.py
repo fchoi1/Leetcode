d29fbefe110dadfc1aa55504117754f648a052c5
Fab <42979283+fchoi1@users.noreply.github.com>
2024-07-13 21:21:06 -0400

    Time: 804 ms (7.07%), Space: 57 MB (87.45%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.py
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -6,15 +6,17 @@ class Solution:
         curr = 0
         for a,t in customers:
             if q:
+                # while q[0][1] > a
+                #     start,end = q.pop()
+                #     total += (end - start)
                 q.append((a, max(q[-1][1], a) + t))
-   
             else:
                 q.append((a, a+t))
         
         while q:
             start, end = q.pop()
             total += (end - start)
-        print("total", total)
+        # print("total", total)
         return total / N
 
         
