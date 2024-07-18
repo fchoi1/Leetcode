60142066da50f9ca5193cf6aff6e73f481b572ba
Fab <42979283+fchoi1@users.noreply.github.com>
2024-07-17 23:17:10 -0400

    Time: 241 ms (12.32%), Space: 18.2 MB (6.77%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        graph = collections.defaultdict(list)
+        
+        def dfs(node, par = None):
+            if node:
+                graph[node].append(par)
+                graph[par].append(node)
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+        dfs(root)
+        
+        leaves = []
+        for node in graph.keys():
+            if node and not node.left and not node.right:
+                leaves.append(node)
+        
+        count = 0
+        
+        for leaf in leaves:
+            queue = [(leaf,0)]
+            seen = set(queue)
+            while queue:
+                node,length = queue.pop(0)
+                if length>distance:
+                    break
+                if node:
+                    for nei in graph[node]:
+                        if nei not in seen:
+                            seen.add(nei)
+                            queue.append((nei,length+1))
+                    if node!=leaf and not node.left and not node.right and length<=distance:
+                        count+=1
+                    
+        return count//2
