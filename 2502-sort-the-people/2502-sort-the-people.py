e647dd924660f80d888fa8494e04232af7f1af73
Fab <42979283+fchoi1@users.noreply.github.com>
2024-07-22 20:10:05 -0700

    Time: 97 ms (88.98%), Space: 17.1 MB (44.9%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,4 +1,4 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        return [n for h,n in sorted([(h,n) for h,n in zip(heights,names)], reverse=True) ]
+        return [ n for h,n in sorted([(h,n) for h,n in zip(heights,names)], reverse=True) ]
         
