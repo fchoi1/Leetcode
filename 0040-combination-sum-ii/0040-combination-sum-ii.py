24044a3a81d479a26476d3b12f3234b731c39908
Fab <42979283+fchoi1@users.noreply.github.com>
2024-08-12 18:12:19 -0700

    Time: 55 ms (72.49%), Space: 16.6 MB (63.92%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,21 +1,27 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        res = []
-        def backtrack(index, currSum, arr):
-            if currSum > target:
-                return
-            if currSum == target:
-                res.append(arr[:])
 
-            for i in range(index, len(candidates)):
+        candidates.sort()
+        sol = []
+        N = len(candidates)
+        def backtrack(index, arr, currSum):
+            nonlocal sol,N
+
+            if currSum >= target  or index >= N:
+                if currSum > target:
+                    return
+                if currSum == target:
+                    sol.append(arr[:])
+
+            for i in range(index, N):
                 if i > index and candidates[i] == candidates[i-1]:
                     continue
                 arr.append(candidates[i])
-                backtrack(i+1, currSum + candidates[i], arr)
+                backtrack(i + 1, arr, currSum + candidates[i])
                 arr.pop()
-
-        backtrack(0,0,[])
-
-        return res
         
+        backtrack(0,[],0)
+        return sol
+            
+            
+                
