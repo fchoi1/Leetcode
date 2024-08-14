ede6c8d55f72178b1569eab967ae0e1f35ad86f3
Fab <42979283+fchoi1@users.noreply.github.com>
2024-08-13 20:09:37 -0700

    Time: 84 ms (85.5%), Space: 17.4 MB (67.27%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
@@ -1,39 +1,32 @@
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        def count_pairs(nums, max_dist) -> int:
+            count = 0
+            N = len(nums)
+            left = 0
+
+            for right in range(N):
+                while nums[right] - nums[left] > max_dist:
+                    left += 1
+                count += right - left
+            return count
+
         nums.sort()
         array_size = len(nums)
 
-        # Initialize binary search range
-        low = 0
-        high = nums[array_size - 1] - nums[0]
+        l = 0
+        r = nums[array_size - 1] - nums[0]
 
-        while low < high:
-            mid = (low + high) // 2
+        while l < r:
+            mid = (l + r) // 2
 
-            # Count pairs with distance <= mid
-            count = self._count_pairs_with_max_distance(nums, mid)
+            count = count_pairs(nums, mid)
 
-            # Adjust binary search bounds based on count
             if count < k:
-                low = mid + 1
+                l = mid + 1
             else:
-                high = mid
+                r = mid
 
-        return low
+        return l
 
-    # Count number of pairs with distance <= max_distance using a moving window
-    def _count_pairs_with_max_distance(
-        self, nums: List[int], max_distance: int
-    ) -> int:
-        count = 0
-        array_size = len(nums)
-        left = 0
-
-        for right in range(array_size):
-            # Adjust the left pointer to maintain the window with distances <=
-            # max_distance
-            while nums[right] - nums[left] > max_distance:
-                left += 1
-            # Add the number of valid pairs ending at the current right index
-            count += right - left
-        return count
+  
