4bb17c196cd7fbf61bda51029dcf34ef17bc9802
Fab <42979283+fchoi1@users.noreply.github.com>
2024-08-21 20:54:06 -0700

    Time: 31 ms (78.63%), Space: 16.3 MB (99.24%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,5 +1,3 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        val = num ^ (2 ** num.bit_length() - 1 )
-        print(bin(val))
-        return val
+        return num ^ (2**(num.bit_length()) - 1)
