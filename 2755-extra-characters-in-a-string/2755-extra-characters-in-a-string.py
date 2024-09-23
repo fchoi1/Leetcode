b46b890b56e9bd9117c73212e6c6b601ba985f8e
Fab <42979283+fchoi1@users.noreply.github.com>
2024-09-22 20:42:42 -0400

    Time: 145 ms (96.67%), Space: 17.4 MB (24.52%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
@@ -1,20 +1,38 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+    def insert(self,word):
+        head = self
+        for letter in word:
+            if letter in head.children:
+                head = head.children[letter]
+            else:
+                head.children[letter] = Trie()
+                head = head.children[letter]
+        head.isEnd = True
+
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        minChar = inf
-        wordDict = set(dictionary)
-        
-        @cache
-        def backtrack(word, minChar):
-            if not word:
-                return minChar
-            minC = inf
-            for i in range(1,len(word)+1):
-                if word[:i] not in wordDict:
-                    minC = min(minC,backtrack(word[i:],minChar + i))
-                else:
-                    minC = min(minC, backtrack(word[i:],minChar))
-            return minC
-        return backtrack(s, 0)
-                
+        trie_head = Trie()
+        #Create Trie
+        for word in dictionary:
+            trie_head.insert(word)
+        # trie_head.printTrie()
+        dp = [None for _ in range(len(s)+1)]
+        dp[-1] = 0
+
+        for start in range(len(s)-1,-1, -1):
+            dp[start] = dp[start+1]+1
+            node = trie_head
+            for end in range(start,len(s)):
+                if s[end] not in node.children:
+                    break
+                node  = node.children[s[end]]
+                if node.isEnd:
+                    dp[start] = min(dp[start],dp[end+1])
+        return dp[0]
 
         
