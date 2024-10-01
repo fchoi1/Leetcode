b027c63c4968f1cd643b2ddb996a94253308fd82
Fab <42979283+fchoi1@users.noreply.github.com>
2024-09-30 20:37:16 -0400

    Time: 485 ms (76.04%), Space: 30.6 MB (20.05%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -6,10 +6,10 @@ class Solution:
 
         if 0 in counts and counts[0] % 2 != 0:
             return False
+        del counts[0]
 
         for n, c in counts.items():
-            diff = k - n if n else 0
+            diff = k - n
             if c != counts[diff]:
                 return False
-
         return True
