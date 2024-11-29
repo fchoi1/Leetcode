fd90a411e55691a5cf7ffa2789bd6849beccf18a
Fab <42979283+fchoi1@users.noreply.github.com>
2024-11-28 21:50:05 -0500

    Time: 1963 ms (9.73%), Space: 43.4 MB (17.52%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -6,7 +6,7 @@ class Solution:
         seen = set()
         h = len(grid)
         w = len(grid[0])
-        
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
         if grid[0][1] > 1 and grid[1][0] > 1:
             return -1
 
@@ -14,19 +14,15 @@ class Solution:
             steps, x, y = heapq.heappop(q)
             if (x,y) == (w-1, h-1):
                 return steps
-            
             if (x,y) in seen:
                 continue
             seen.add((x,y))
 
-            for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
+            for dx, dy in directions:
                 newX = x + dx
                 newY = y + dy
                 if 0 <= newX < w and 0 <= newY < h:
-                    if steps + 1 >= grid[newY][newX]:
-                        heapq.heappush(q, (steps + 1, newX, newY))
-                    else:
-                        diff = grid[newY][newX] - steps + 1
-                        heapq.heappush(q, (grid[newY][newX] + int(diff % 2), newX, newY))
+                    nextStep = max(steps + 1, grid[newY][newX] + int((grid[newY][newX] - steps + 1) % 2))
+                    heapq.heappush(q, (nextStep, newX, newY))
         return -1
         
