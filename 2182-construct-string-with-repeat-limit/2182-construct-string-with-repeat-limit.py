570bf6527662e098c5efd43fd2f889528a6bf57c
Fab <42979283+fchoi1@users.noreply.github.com>
2024-12-16 20:38:07 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -1,48 +1,43 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+
         letters = [0] * 26
-
-        # bucket sort
         for char in s:
-            letters[ord("z")-ord(char)] += 1
+            letters[ord('z') - ord(char)] += 1
 
-        # replacement pointer 
         fast = 0
-        string = ""
+        string = ''
+        print(letters)
         for i in range(26):
             if letters[i] == 0:
                 continue
             char = chr(ord("z") - i)
+            print("char",char, i)
+            
 
-            # Update replacement pointer to a different non-empty character than current         
-            while fast < len(letters) and (fast == i or letters[fast] == 0):
+            while fast < len(letters) and  (fast <= i or letters[fast] == 0):
                 fast += 1
             
-            # get cycles and remainder
-            subtract, remain = divmod(letters[i], repeatLimit)
-            
-            for i in range(subtract):
+            times, remain = divmod(letters[i], repeatLimit)
+            if not remain:
+                times -= 1
+
+            for j in range(times):
                 string += char * repeatLimit
 
-                # edge case when remainder is zero, no need to add replacement pointer
-                if i == subtract - 1 and remain == 0:
-                    break
-
-                # Get next replacement pointer
                 while fast < len(letters) and letters[fast] ==  0:
                     fast += 1
-                
-                # return string if there is no more replacement pointer available
+                    
+                # No more replacement letters to add
                 if fast >= len(letters):
                     return string
                 
-                # add replacement letter
-                string += chr(ord("z") - fast)
-
-                # Subtract from letters since we used a replacement letter
+                string += chr(ord('z') - fast)
                 letters[fast] -= 1
-                
-            # add remainder is any
+
             string += char * remain
+
                 
         return string
+
+        # heap
