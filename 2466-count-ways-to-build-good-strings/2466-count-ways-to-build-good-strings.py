2e6233df8f23a255c4a939229eb5bec50a3d5855
Fab <42979283+fchoi1@users.noreply.github.com>
2025-01-03 00:49:17 -0500

    Time: 1017 ms (5.91%), Space: 684.1 MB (5.24%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -2,19 +2,14 @@ class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
         
 
-        # low, high
-        # zero, one 
-        # zero zero
-        # one one
-        # one zero
-
-        # recursion with cache
-        # dp 
         dp = [0] * (high + 1)
         dp[zero] += 1
         dp[one] += 1
 
-        for i in range(high):
+        curr = 0
+        modulo = 10**9 + 7
+
+        for i in range(high + 1):
             # zero
             zLen = i + zero
             oLen = i + one
@@ -22,13 +17,10 @@ class Solution:
                 dp[zLen] += dp[i]
             if oLen <= high:
                 dp[oLen] += dp[i]
-            # print(zLen, oLen, 'index', i, dp)
+            if i >= low :
+                curr += dp[i] 
 
-
-        #     # one
-        # print(dp)
-        modulo = 10**9 + 7
-        return sum(dp[low:high+1]) % modulo
+        return curr % modulo
 
 
 
