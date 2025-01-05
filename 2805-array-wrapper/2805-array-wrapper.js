560400e5446a33adf4553912c54b977b7b7692b2
Fab <42979283+fchoi1@users.noreply.github.com>
2025-01-05 10:24:52 -0800

    Time: 46 ms (89.77%), Space: 49.9 MB (89.06%) - LeetHub

---
+++ 2805-array-wrapper/2805-array-wrapper.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.arr = nums
+    this.val = nums.reduce((a,b) => a + b, 0)
+    
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.val   
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.arr) 
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
