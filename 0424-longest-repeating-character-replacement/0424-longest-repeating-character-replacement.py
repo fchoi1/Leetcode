197cc8996e31750556f0aa5bf6f07613bf173640
Fab <42979283+fchoi1@users.noreply.github.com>
2025-01-06 17:36:03 -0800

    Time: 197 ms (17.29%), Space: 13.9 MB (100%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,21 +1,57 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+      
+#       subsetCount = [0] * 26
+#       maxCount = 0
+#       maxLength = 0
+#       startIndex = 0
+#       endIndex = 0
+      
+#       for i in range(len(s)):
 
-        slow = fast = 0
-        counts = [0] * 26
-        maxLen = k
+#         while (endIndex - startIndex) - maxCount <= k:
+#           subsetCount[ord(s[endIndex]) - ord('A')] += 1
+#           maxCount = max(maxCount, subsetCount[ord(s[endIndex]) - ord('A')])
+#           maxLength = max(maxLength, endIndex - startIndex)
+#           endIndex += 1
 
-        while fast < len(s):
-            char = ord(s[fast]) - ord("A")
-            counts[char] += 1
-            while sum(counts) - max(counts) > k:
-                index = ord(s[slow]) - ord("A")
-                counts[index] -= 1
-                slow += 1
-            maxLen = max(maxLen, fast - slow + 1)
-            fast += 1
-        return maxLen
+#           if endIndex >= len(s) and (endIndex - startIndex) - maxCount <= k:
+#             maxLength = max(maxLength, endIndex - startIndex)
+#             return maxLength
+#           elif endIndex >= len(s):
+#             return maxLength
+          
+#         subsetCount[ord(s[startIndex]) - ord('A')] -= 1
+#         maxCount = max(subsetCount)
+#         startIndex += 1
 
+#       return maxLength
+    
+      subsetCount = [0] * 26
+      maxCount = 0
+      maxLength = 0
+      startIndex = 0
+      endIndex = 0
 
-            
+      while endIndex < len(s):
+        
+        if (endIndex - startIndex) - maxCount <= k:
+          
+          subsetCount[ord(s[endIndex]) - ord('A')] += 1
+          maxCount = max(maxCount, subsetCount[ord(s[endIndex]) - ord('A')])
+          maxLength = max(maxLength, endIndex - startIndex)
+          endIndex += 1
+
+        else:
+          subsetCount[ord(s[startIndex]) - ord('A')] -= 1
+          maxCount = max(subsetCount)
+          startIndex += 1
+          
+      if (endIndex - startIndex) - maxCount <= k:
+        maxLength = max(maxLength, endIndex - startIndex)
+      return maxLength 
+        
+      
+      
+      
         
