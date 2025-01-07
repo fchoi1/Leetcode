6c57d04785812e02353d3bbde8c669b771cb6ec5
Fab <42979283+fchoi1@users.noreply.github.com>
2025-01-06 17:34:08 -0800

    Time: 330 ms (7.65%), Space: 18 MB (28.44%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,19 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
-        # 2 pointers
-        slow = 0
+        slow = fast = 0
         counts = [0] * 26
         maxLen = k
 
-        for i in range(len(s)):
-            char = ord(s[i]) - ord("A")
+        while fast < len(s):
+            char = ord(s[fast]) - ord("A")
             counts[char] += 1
             while sum(counts) - max(counts) > k:
                 index = ord(s[slow]) - ord("A")
                 counts[index] -= 1
                 slow += 1
-            maxLen = max(maxLen, i - slow + 1)
+            maxLen = max(maxLen, fast - slow + 1)
+            fast += 1
         return maxLen
 
 
