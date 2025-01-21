d7bf8a8aee32f7f3da8341c1f5048f23e3f791fb
Fab <42979283+fchoi1@users.noreply.github.com>
2025-01-20 17:04:52 -0800

    Time: 139 ms (24.59%), Space: 31.2 MB (6.23%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -1,37 +1,25 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
 
-
-        N =  len(grid[0])
-        def getPrefix(arr):
-            pre_1 = []
-            pre_2 = []
-            for a,b in zip(arr[0], arr[1][::-1]):
-                if not pre_1:
-                    pre_1.append(a)
-                    pre_2.append(b)
-                else:
-                    pre_1.append(pre_1[-1] + a)
-                    pre_2.append(pre_2[-1] + b)
-            pre_2.reverse()
-            return pre_1, pre_2, pre_1[-1], pre_2[0]
-
-        # get switch index
-        top, bot, top_total, bot_total = getPrefix(grid)
-        switch = 0
-        curr = top[0] + bot[0]
-        p2_curr = 0
+        top = []
+        bot = []
+        for a,b in zip(grid[0], grid[1][::-1]):
+            if not top:
+                top.append(a)
+                bot.append(b)
+            else:
+                top.append(top[-1] + a)
+                bot.append(bot[-1] + b)
+        bot.reverse()
+        
+        top_total = top[-1]
+        bot_total = bot[0]
         p2_min = inf
+
         for i, (a,b) in enumerate(zip(top,bot)):
-
-
-            t = top_total - a
-            b = bot_total - b
-
-            p2_min = min(p2_min,max(t,b))
-            # bot
-            if a + b > curr:
-                curr = a + b
-                switch = i
+            path_1 = top_total - a
+            path_2 = bot_total - b
+            p2_min = min(p2_min,max(path_1, path_2))
+       
         return p2_min
 
