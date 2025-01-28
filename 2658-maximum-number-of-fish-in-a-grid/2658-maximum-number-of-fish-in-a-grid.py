9193107ee1e3b3ba5cb2ec22f63f3e3f1b57829f
Fab <42979283+fchoi1@users.noreply.github.com>
2025-01-27 17:35:07 -0800

    Time: 252 ms (5.12%), Space: 20.9 MB (10%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        
+        # bfs
+        def getfish(q):
+            fish = 0
+            while q:
+                x,y = q.popleft()
+
+                fish += grid[y][x]
+                grid[y][x] = 0
+                
+                for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
+                    newX, newY = x + dx, y + dy
+                    if H > newY >= 0 and W > newX >= 0 and grid[newY][newX] > 0:
+                        q.append((newX, newY))
+            return fish
+                    
+        # largest island value?
+
+        W = len(grid[0])
+        H = len(grid)
+
+        maxFish = 0
+        for y in range(H):
+            for x in range(W):
+                if grid[y][x]:
+                    maxFish = max(maxFish, getfish(deque([(x,y)])))
+        return maxFish
+
+        
+                
+
