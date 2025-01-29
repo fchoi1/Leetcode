466127450cab57614b1f0031e7f85f1146d7ceea
Fab <42979283+fchoi1@users.noreply.github.com>
2025-01-28 20:06:39 -0800

    Time: 9 ms (21.67%), Space: 19.2 MB (6.12%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,48 +1,35 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        def dfs(node, path, seen, prev,):
-            nonlocal loop
-            if loop:
-                return
-            if node in seen:
-                loop = path.copy()
-                return
-            seen.add(node)
-            for n in adjMap[node]:
-                if n == prev:
-                    continue
-                path.append(n)
-                dfs(n, path, seen, node)
-                path.pop()
-            seen.remove(node)
-
-        # Adj Map
+        
         adjMap = defaultdict(set)
         for a,b in edges:
             adjMap[a].add(b)
             adjMap[b].add(a)
+
         
-        # Find loop path
-        loop = None
-        for i in range(len(edges)):
-            if loop:
-                break
-            dfs(i+1, [i+1], set(), None)
+        # get cycle and check edge
+        self.seen = {}
+        self.cycle = None
 
-        # find acutally loop
-        for i, n in enumerate(loop):
-            if n == loop[-1]:
-                loop = loop[i:]
-                break
+        def dfs(node, path):
+            if node in self.seen:
+                if not self.cycle:
+                    self.cycle = path[self.seen[node]:] + [node]
+                return
+            self.seen[node] = len(path)
+            for n in adjMap[node]:
+                if path and n == path[-1]:
+                    continue
+                dfs(n, path + [node])
+        dfs(1,[])
 
-        # create edges set
-        edgesSet = set()
-        for a,b in zip(loop, loop[1:]):
-            edgesSet.add((min(a,b), max(a,b)))
-        edgesSet.add((min(loop[0], loop[-1]), max(loop[0], loop[-1])))
+        e = set()
+        for a,b in zip(self.cycle, self.cycle[1:]):
+            e.add((min(a,b),max(a,b)))
+        
+        remove = None
+        for a,b in edges:
+            if (min(a,b), max(a,b)) in e:
+                remove = (a,b)
 
-        extra = None
-        # get first matching reverse
-        for a,b in edges[::-1]:
-            if (min(a,b), max(a,b)) in edgesSet:
-                return [a,b]
+        return remove
