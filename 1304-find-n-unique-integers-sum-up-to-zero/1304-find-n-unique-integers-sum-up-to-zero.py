37fd930e10f25ddf9dbde0e33f9cc18e3dee624e
Fab <42979283+fchoi1@users.noreply.github.com>
2025-02-06 22:27:57 -0800

    Time: 0 ms (100%), Space: 17.9 MB (28.89%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -1,8 +1,9 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        even = n % 2 == 0
-        pos = list(range(1,n//2+1))
-        neg = list(range(-1, -n//2-int(even), -1))
-        return pos + neg if even else pos + neg + [0]
-
-        
+        ans = []
+        for i in range(1,n//2+1):
+            ans.append(i)
+            ans.append(-i)
+        if n % 2 == 1:
+            ans.append(0)
+        return ans
