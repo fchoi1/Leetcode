777af34616415e231e28622f200ceb3585f4799b
Fab <42979283+fchoi1@users.noreply.github.com>
2025-02-18 22:31:25 -0800

    Time: 59 ms (90.03%), Space: 31 MB (96.95%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -1,27 +1,32 @@
 class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
-        farmland = []
-
-        H = len(land)
         W = len(land[0])
+        H = len(land)
 
-        def traverse(x,y,botX, botY):
-            if x < 0 or x >= W or y < 0 or y >= H or not land[y][x]:
-                return (botX, botY)
-
-            land[y][x] = 0 
-            botX, botY = max(botX, x),max(botY, y)
-
-            for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
-                botX, botY = traverse(x+dx,y+dy, botX, botY)
-            return (botX, botY)
-
-
+        def search(x,y):
+            while y < H and land[y][x]:
+                y += 1
+            y -= 1
+            while x < W and land[y][x]:
+                x += 1
+            x -= 1
+            return x,y
+            
+        # left up corners
+       
+        groups = []
         for y in range(H):
             for x in range(W):
-                if land[y][x]:
-                    botX, botY = traverse(x,y,x,y)
-                    farmland.append([y,x,botY,botX])
+                if not land[y][x]:
+                    continue
+                isLeftCorner = (
+                    (x == 0 and y == 0 and land[y][x]) or
+                    (x == 0 and not land[y-1][x]) or
+                    (y == 0 and not land[y][x-1]) or
+                    (not land[y][x-1] and not land[y-1][x])
+                )
+                if isLeftCorner:
+                    nx, ny = search(x,y)
+                    groups.append((y,x,ny,nx))
 
-        return farmland
-        
+        return groups
