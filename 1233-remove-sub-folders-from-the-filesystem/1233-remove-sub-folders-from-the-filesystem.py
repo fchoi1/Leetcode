db6037413366f6ed4bd2f140fef7815d273053c0
Fab <42979283+fchoi1@users.noreply.github.com>
2025-02-18 21:49:08 -0800

    Time: 48 ms (60.24%), Space: 31.5 MB (39.65%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -6,24 +6,20 @@ class Trie:
 
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        # sort by number of /
-        # Trie? 
+    
         folder.sort(key=lambda x: x.count('/'))
         root = Trie()
         ans = []
         for f in folder:
             curr = root
             path = f.split('/')[1:]
-            # print("PATH", path)
             for name in path:
                 if name not in curr.children:
                     curr.children[name] = Trie(name)
-             
                 curr = curr.children[name]
                 if curr.end:
                     break
             else:
-                # print("append to ans", path, curr.name)
                 curr.end = True
                 ans.append(f)
         return ans
