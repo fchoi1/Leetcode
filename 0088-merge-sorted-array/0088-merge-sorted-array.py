0a61a53442e88947b5a02895282e8176280e1303
Fab <42979283+fchoi1@users.noreply.github.com>
2025-02-19 17:04:18 -0800

    Time: 0 ms (100%), Space: 17.6 MB (93.49%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,21 +1,25 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # the m length in nums1 can be used to swap
 
-        last = len(nums1) - 1
-        ptr2 = n - 1
-        ptr1 = m - 1
+        i1 = m - 1
+        i2 = n - 1
+        i = m + n - 1
 
-        while ptr1 >= 0 and ptr2 >= 0:
-            if nums1[ptr1] <= nums2[ptr2]:
-                nums1[last] = nums2[ptr2]
-                ptr2 -= 1
+        while i1 >= 0 and i2 >= 0:
+            if nums1[i1] > nums2[i2]:
+                nums1[i] = nums1[i1]
+                i1 -= 1
             else:
-                nums1[last] = nums1[ptr1]
-                ptr1 -= 1
-            last -= 1
+                nums1[i] = nums2[i2]
+                i2 -= 1
+            i -= 1
 
-        while ptr2 >= 0:
-            nums1[last] = nums2[ptr2]
-            ptr2 -= 1
-            last -= 1
+        while i2 >= 0:
+            nums1[i] = nums2[i2]
+            i -= 1
+            i2 -= 1
 
