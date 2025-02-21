fe2ae0566d2da077cba5d729759fc42a31379e7c
Fab <42979283+fchoi1@users.noreply.github.com>
2025-02-21 00:44:55 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -8,19 +8,18 @@ class Solution:
             if i == N - 1:
                 break
             currSteps = nums[i]
-            if i + currSteps >= N - 1:
-                return steps + 1
-            print("at index", i, currSteps)
+            # if i + currSteps >= N - 1:
+            #     return steps + 1
 
             furthest = currI = 0
+
             for j in range(currSteps):
-                print("loop", j)
                 if j + i + 1 >= N:
-                    break
+                    return steps + 1
+
                 if i + j + nums[j + i + 1] + 1 > furthest:
                     currI = j + 1
                     furthest = i + j + nums[j + i + 1] + 1 
-                    print("found furthest", nums[j + i + 1], "currI",  currI )
             i += currI
             steps += 1
         return steps
