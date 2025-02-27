5fbda77143f088ee341e8548df92c68df0f32148
Fab <42979283+fchoi1@users.noreply.github.com>
2025-02-26 22:49:11 -0500

    Time: 315 ms (56.37%), Space: 17.8 MB (61.8%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,33 +1,39 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        # b a b a d
-        def checkPali(s, left, right):
-          
-          while left > 0 and right+1 < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-            if s[left] != s[right]:
-              left += 1
-              right -= 1
-              break
+        
+        N = len(s)
+        if N == 1:
+            return s
 
-          return left, right+1
-        
-        start = 0
-        end = 1
-        for i in range(len(s)):
-          if i+1 < len(s) and s[i] == s[i+1]:
-            doubleL, doubleR = checkPali(s, i, i+1)
-            if  doubleR - doubleL > end - start:
-              start = doubleL
-              end = doubleR
-          
-          singleL, singleR = checkPali(s, i, i)
-          if singleR - singleL > end - start:
-              start = singleL
-              end = singleR
-        
-        return s[start:end]
-          
-          
+        def getPali(l,r):
+            if s[l] != s[r]:
+                return (l,r)
+
+            while s[l] == s[r] and r < N - 1 and l > 0:
+                l -= 1 
+                r += 1
+                if s[l] != s[r]:
+                    l += 1
+                    r -= 1
+                    break
+
+            return (l, r + 1)
+
+        longest = 0
+        ans = ''
+        for i in range(N):
+
+            l_odd,r_odd = getPali(i, i) 
+            if i > 0:
+                l_even,r_even = getPali(i, i-1) 
+                if r_even - l_even > longest:
+                    longest = r_even - l_even
+                    ans = s[l_even:r_even]
+                
+                
+            if r_odd - l_odd > longest:
+                longest = r_odd - l_odd
+                ans = s[l_odd:r_odd]
+
+        return ans 
