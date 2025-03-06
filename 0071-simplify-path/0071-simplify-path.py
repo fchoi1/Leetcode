1042892f1507518b08ab0d67c9a2d88641b307f5
Fab <42979283+fchoi1@users.noreply.github.com>
2025-03-05 21:40:55 -0500

    Time: 9 ms (6.34%), Space: 17.9 MB (47.94%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,41 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+
+        def getNextFolder(index):
+
+            while index < len(path) and path[index] == '/':  # index = 5 -> 7
+                index += 1
+
+            name = ''
+            while index < len(path) and path[index] != '/': # index = 1 -> 5 | index = 7 -> 10
+                name += path[index]
+                index += 1
+
+            return (name, index) # home, 5 | foo, 10
+
+
+
+        index = 0
+        while index < len(path): # 0
+
+            char = path[index] # 
+            if char == '/':
+                nextFolder, index = getNextFolder(index) # home, 5 | foo, 10
+
+                if not nextFolder: # reach the end
+                    break
+
+                if nextFolder == '.':
+                    continue
+
+                if nextFolder == '..' and stack:
+                    stack.pop()
+                elif nextFolder != '..':
+                    stack.append(nextFolder) # [home, foo]
+            else:
+                print("err")
+
+
+        return "/" + "/".join(stack)
+        
