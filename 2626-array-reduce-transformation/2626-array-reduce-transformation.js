8d2570b7a083094a293180c87be84577380d2311
Fab <42979283+fchoi1@users.noreply.github.com>
2025-03-19 21:06:49 -0700

    Time: 45 ms (73.44%), Space: 55.6 MB (8.71%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    return nums.reduce((acc, cur) => fn(acc,cur), init)
+};
