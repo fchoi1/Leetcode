2b574af037f1600f247f8ec26e3f37b5dfd84a89
Fab <42979283+fchoi1@users.noreply.github.com>
2025-03-30 21:24:12 -0700

    Time: 12 ms (79.21%), Space: 18.1 MB (48.21%) - LeetHub

--- 1483-rank-teams-by-votes/1483-rank-teams-by-votes.py
+++ 1483-rank-teams-by-votes/1483-rank-teams-by-votes.py
@@ -1,16 +1,15 @@
 class Solution:
     def rankTeams(self, votes: List[str]) -> str:
-
     
         N = len(votes[0])
         counts = [[0 for _ in range(26)] for _ in range(N)]
 
+        
+
         for v in votes:
             for i,c in enumerate(v):
                 counts[i][ord(c) - ord('A')] += 1
-
-        for row in counts:
-            print(row)
+    
         # organzie into tuples
         sorted_v = []
         for i in range(26):
