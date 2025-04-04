1d4edbb63a654791e2816d82b139b4a4f8cd08ed
Fab <42979283+fchoi1@users.noreply.github.com>
2025-04-03 17:25:52 -0700

    Time: 7 ms (20.36%), Space: 18.8 MB (6.81%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -6,27 +6,27 @@
 #         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # get deepest
 
-        # for each deepest, we need the path,  after we check and zip until. 
-        # dfs
-
-        # dfs to get the deepest, 
         self.depth = 0
         self.lca = None
+        self.cache = {}
+
         def isDeepest(node, d):
+            if (node,d) in self.cache:
+                return self.cache[(node,d)]
 
             if not node:
                 self.depth = max(self.depth, d)
                 return d  
 
-            
             left = isDeepest(node.left, d + 1)
             right = isDeepest(node.right, d + 1)
             
             if left == right and left == self.depth:
                 self.lca = node
-            return max(left, right)
+
+            self.cache[(node,d)] = max(left, right)
+            return self.cache[(node,d)]
 
         isDeepest(root,0)
         return self.lca
