7a4aca4d0ec224fd862f8e23b217fe1c80953db7
Fab <42979283+fchoi1@users.noreply.github.com>
2025-04-27 18:24:23 -0700

    Time: 99 ms (98.47%), Space: 30.6 MB (31.97%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
@@ -1,23 +1,17 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
 
-        # prefix sum
-        prefix = [0]
-        for n in nums:
-            prefix.append(prefix[-1] + n)
         
         N = len(nums)
         total = N * (N+1)//2
-        l = c = 0
+        l = c = curr = 0
 
-        print("PRE", prefix)
         for r, n in enumerate(nums):
-            score = (prefix[r + 1] - prefix[l]) * (r - l + 1)
-            while score >= k:
+            curr += n
+            while curr * (r - l + 1) >= k:
                 c += N - r
-                l += 1
-                score = (prefix[r + 1] - prefix[l]) * (r - l + 1)
-     
+                curr -= nums[l]
+                l += 1     
         return total - c
 
         
