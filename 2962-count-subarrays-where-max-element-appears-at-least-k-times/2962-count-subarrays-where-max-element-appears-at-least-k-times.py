608537e01c376a2d90c33ab024461e0809d7fc9b
Fab <42979283+fchoi1@users.noreply.github.com>
2025-04-28 19:20:40 -0700

    Time: 93 ms (39.69%), Space: 29.4 MB (90.15%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,22 +1,9 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-
-        # heap 
-        # hash
-
-        # once we find an array that satisfies it, the rest do as well and all previous ones too
-        # might well count the subarrays that dont have k
-        #
-
-        # total subarrays len(n)
         n = len(nums)
         maxEl = max(nums)
-        print("len", n)
         total = n * (n+1) // 2
-        
-        counts = 0
-        slow = 0
-        neg = 0
+        counts = slow = neg = 0
         for i, n in enumerate(nums):
             if n == maxEl:
                 counts += 1
@@ -26,12 +13,6 @@ class Solution:
                 slow += 1
             neg += i - slow + 1
         return total - neg
-
-
-        # 1 3 1 2
-        # 
-            
-
 
 
 
