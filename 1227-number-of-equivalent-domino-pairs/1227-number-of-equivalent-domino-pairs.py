3ba1ec4748fcda8d54d9623614110b68fbfd2765
Fab <42979283+fchoi1@users.noreply.github.com>
2025-05-03 17:49:03 -0700

    Time: 12 ms (53.07%), Space: 24.2 MB (81.31%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
@@ -1,10 +1,11 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
         counts = defaultdict(int)
-        total = 0
         for a,b in dominoes:
             counts[(min(a,b), max(a,b))] += 1
-            total += counts[(min(a,b), max(a,b))] - 1
-            
+            # total += counts[(min(a,b), max(a,b))] - 1
+        total = 0
+        for c in counts.values():
+            total += c * (c - 1) // 2
         return total
         
