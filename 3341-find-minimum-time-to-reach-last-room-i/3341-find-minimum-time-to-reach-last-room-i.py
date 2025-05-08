520a114929f08f4ee53c1f7ece7ad3a6c325e6d5
Fab <42979283+fchoi1@users.noreply.github.com>
2025-05-07 17:15:08 -0700

    Time: 114 ms (96.85%), Space: 18.7 MB (12.34%) - LeetHub

--- 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.py
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.py
@@ -1,6 +1,5 @@
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        # =dijstras
 
         heap = [(0,0,0)] # time, x, y
         H = len(moveTime)
@@ -12,13 +11,12 @@ class Solution:
             if (x,y) == (W-1, H-1):
                 return currTime
 
-            if (x,y) in seen:
-                continue
-            seen.add((x,y))
-
             for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
                 nx = x + dx
                 ny = y + dy
+                if (nx,ny) in seen:
+                    continue
+                seen.add((nx,ny))
                 if 0 <= nx < W and 0 <= ny < H:
                     heapq.heappush(heap, ( max(currTime,moveTime[ny][nx]) + 1, nx, ny))
         
