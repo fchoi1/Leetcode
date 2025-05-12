2fabb594116ac45b85c12c2e5fa5e44533276c4d
Fab <42979283+fchoi1@users.noreply.github.com>
2025-05-11 17:46:40 -0700

    Time: 220 ms (47.04%), Space: 21.8 MB (7.06%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
@@ -1,18 +1,22 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        
-        counts = Counter(digits)
+        # three digits
+        numList = []
+        digits.sort()
+        seen = set()
+        def getPermutations(numStr, digits):
+            if numStr in seen:
+                return
+            seen.add(numStr)
+            if len(numStr) == 3:
+                num = int(numStr)
+                if num >= 100 and num % 2 == 0:
+                    numList.append(num)
+                return
 
-        valid = []
-
-        for i in range(100, 1000, 2):
-            curr = Counter(list(str(i)))
-
-            for k,v in curr.items():
-                if counts[int(k)] < v:
-                    break
-            else:
-                valid.append(i)
-        
-        return valid
-            
+            for i,n in enumerate(digits):
+                temp = digits.copy()
+                temp.pop(i)
+                getPermutations(numStr + str(n),temp)
+        getPermutations('', digits)
+        return numList
