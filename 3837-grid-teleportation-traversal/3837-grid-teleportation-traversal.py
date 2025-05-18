d72f84b311d02f48333f49ae3c04ad129857662d
Fab <42979283+fchoi1@users.noreply.github.com>
2025-05-17 21:15:31 -0700

    Time: 6795 ms (19.08%), Space: 230.8 MB (5.32%) - LeetHub

--- 3837-grid-teleportation-traversal/3837-grid-teleportation-traversal.py
+++ 3837-grid-teleportation-traversal/3837-grid-teleportation-traversal.py
@@ -12,12 +12,13 @@ class Solution:
                     portal[val].add((x,y))
      
         
-        q = deque([(0,0,0, set())]) # cost, x,y , portal
+        q = deque([(0,0,0)]) # cost, x,y , portal
 
         steps = 0
         seen = {}
+        used = set()
         while q:            
-            cost,x,y,letters = q.popleft()
+            cost,x,y = q.popleft()
             cell = matrix[y][x]
 
             if (x,y) == (W-1, H-1):
@@ -28,19 +29,18 @@ class Solution:
                 
             seen[(x,y)] = cost
 
-            if cell not in letters:
+            if cell not in used:
+                used.add(cell)
                 for (lx, ly) in portal[cell]:
                     if (lx, ly) == (W-1, H-1):
                         return cost
-                    t = letters.copy()
-                    t.add(cell)
-                    q.appendleft((cost, lx, ly, t))
+                    q.appendleft((cost, lx, ly))
                     
             for dx, dy in dir:
                 nx = x + dx
                 ny = y + dy
                 if 0 <= nx < W and 0 <= ny < H and matrix[ny][nx] != '#':
-                    q.append((cost + 1, nx, ny, letters))
+                    q.append((cost + 1, nx, ny))
                 
         return -1
                 
