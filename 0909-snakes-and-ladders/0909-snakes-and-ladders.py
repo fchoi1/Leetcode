27e5f18087cd5c8bc650e99234efa3b2c416b8ba
Fab <42979283+fchoi1@users.noreply.github.com>
2025-05-30 23:00:07 -0400

    Time: 6706 ms (5.11%), Space: 18.4 MB (5.35%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,38 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        # climb ladders dfs
+
+        seen = {}
+        N = len(board)
+        MAX_CELL = N ** 2 
+
+   
+        def getNextCell(cell):
+            r , d = divmod(cell - 1 ,  N)
+            
+            if r % 2 == 0:
+                x = d 
+            else:
+                x = N - d - 1
+
+            y = N - 1 - r
+            return board[y][x] if board[y][x] != -1 else cell
+
+        def dfs(cell, step):
+
+            if cell == MAX_CELL:
+                return step 
+
+            if cell in seen and step >= seen[cell]:
+                return inf
+            seen[cell] = step
+            steps = inf
+            # dice
+            for i in range(6):
+                nextCell = getNextCell(min(cell + 1 + i, MAX_CELL))
+                steps = min(steps, dfs(nextCell, step + 1))
+
+            return steps
+
+        steps = dfs(1,0)
+        return steps if steps != inf else -1
