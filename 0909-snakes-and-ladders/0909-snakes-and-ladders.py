5a67094420ae75756e05ea7631189f038cffbfb2
Fab <42979283+fchoi1@users.noreply.github.com>
2025-05-30 23:02:20 -0400

    Time: 61 ms (7.34%), Space: 17.8 MB (82.91%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,27 +1,30 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         N = len(board)
-        
-        def getNext(cell):
+
+        def get_next(cell):
             r, d = divmod(cell - 1, N)
             x = d if r % 2 == 0 else N - 1 - d
             y = N - 1 - r
             return board[y][x] if board[y][x] != -1 else cell
 
         visited = set()
-        queue = deque([(1, 0)])  # (cell, steps)
+        heap = [(0, 1)]  # (steps, cell)
 
-        while queue:
-            cell, steps = queue.popleft()
+        while heap:
+            steps, cell = heapq.heappop(heap)
             if cell == N * N:
                 return steps
+            if cell in visited:
+                continue
+            visited.add(cell)
+
             for i in range(1, 7):
                 next_cell = cell + i
                 if next_cell > N * N:
                     continue
-                dest = getNext(next_cell)
+                dest = get_next(next_cell)
                 if dest not in visited:
-                    visited.add(dest)
-                    queue.append((dest, steps + 1))
-                    
+                    heapq.heappush(heap, (steps + 1, dest))
+
         return -1
