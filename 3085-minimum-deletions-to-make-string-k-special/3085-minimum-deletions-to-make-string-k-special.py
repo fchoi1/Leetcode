62790f7d3c96eaa1a1eb787135e74c564eb63082
Fab <42979283+fchoi1@users.noreply.github.com>
2025-06-21 00:34:14 -0400

    Time: 43 ms (100%), Space: 18.1 MB (45.74%) - LeetHub

--- 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.py
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.py
@@ -1,22 +1,21 @@
 class Solution:
     def minimumDeletions(self, word: str, k: int) -> int:
         counts = Counter(word)
+        minDelete = inf
+
         freq = Counter(counts.values())
-        sortedFreq = sorted(freq.items())
-        left = 0
-        right = len(sortedFreq) - 1
-        
-        steps = inf
-        for i, (freq, numFreq) in enumerate(sortedFreq):
-            currStep = 0
-            for f, nf in sortedFreq[i+1:]:
-                if f > (freq + k):
-                    currStep += (f - (freq + k)) * nf
-         
-            for f, nf in sortedFreq[:i]:
-                currStep += f * nf
-            steps = min(steps, currStep)
-            
-        return steps
-                    
-      
+        sorted_freq = sorted(freq.items())
+        for target, _ in sorted_freq:
+            delete_top = 0
+            delete_bot = 0
+            for key, v in sorted_freq:
+                if key - target > k:
+                    delete_top += v * abs(target - key + k)
+
+                elif target > key:
+                    delete_bot += v * key
+            minDelete = min(minDelete, delete_top +delete_bot)
+
+
+        return minDelete
+    
