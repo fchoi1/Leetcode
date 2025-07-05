1fa292dcb12f91e355cde53e4cce93b9919889b2
Fab <42979283+fchoi1@users.noreply.github.com>
2025-07-04 20:41:21 -0400

    Time: 19 ms (88.88%), Space: 19.2 MB (25.2%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -6,8 +6,4 @@ class Solution:
         for k,v in counts.items():
             if k-1 in counts:
                 res = max(res, counts.get(k-1) + v)
-            
-            if k + 1 in counts:
-                res = max(res, counts.get(k+1) + v)
-        
         return res
