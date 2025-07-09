f04e13828b857e13eb53d357511acd8bccb7bc51
Fab <42979283+fchoi1@users.noreply.github.com>
2025-07-09 01:22:13 -0400

    Time: 49 ms (61.22%), Space: 35.9 MB (87.45%) - LeetHub

--- 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.py
+++ 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.py
@@ -1,13 +1,7 @@
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
-        # move at most k
 
-        # sorted
-
-        startTime.sort()
-        endTime.sort()
         N = len(startTime)
-        # heap or sliding window?
         prevE = 0
         maxFree = 0
         for i in range(k + 1):
@@ -17,9 +11,6 @@ class Solution:
 
             maxFree += s - prevE
             prevE = e
-
-        print("curr Max", maxFree)
-        # ...1.1.1
 
 
         firstS = s
@@ -33,18 +24,15 @@ class Solution:
             if i >= N:
                 currFree += eventTime - prevE
                 currFree -= firstS - firstE
-                print("END", currFree, maxFree, "add", eventTime - prevE, "sub", firstS - firstE, (firstS,firstE))
                 return max(maxFree, currFree)
 
             s,e = startTime[i], endTime[i]
-            
+        
             currFree -= firstS - firstE
-
             currFree += s - prevE
-            print("prevS", firstS, firstE, currFree, "add", s - prevE, 'sub', firstS - firstE)
             maxFree = max(maxFree, currFree)
             
             prevE = e
-        # shouldn't reach?
+        # shouldn't reach ever
         return maxFree
 
