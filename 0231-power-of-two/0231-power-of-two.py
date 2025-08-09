90437d09c2b55afeb47acf8384b71438632bd2a8
Fab <42979283+fchoi1@users.noreply.github.com>
2025-08-08 21:12:44 -0400

    Time: 0 ms (100%), Space: 17.8 MB (55.48%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n < 0:
-            return False
-        return sum(b == '1' for b in bin(n)[2:]) == 1
+        binStr = bin(n)
+        return binStr.count('1') == 1 and n > 0
         
