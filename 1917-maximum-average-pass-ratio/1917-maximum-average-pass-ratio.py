8069edc1d144f874f753cbdbff6e31f1b573cb5d
Fab <42979283+fchoi1@users.noreply.github.com>
2025-08-31 20:18:13 -0400

    Time: 912 ms (98.7%), Space: 54.9 MB (46.96%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
@@ -1,25 +1,30 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        ratios = []
-        perfect = 0
-        for p,c in classes:
-            if c == p:
-                perfect += 1
-                continue
-            inc = (p+1)/(c+1) - p/c
-            heappush(ratios, (-inc, c, p))
 
-        if len(ratios) == 0:
-            return 1
+        # sort by total students and ratio
+        # 
+        h = []
+        r = 0
+        for p,t in classes:
+            if p == t:
+                r += 1
+            else:
+                heappush(h, (p/t-(p+1)/(t+1),t,p))
 
-        for i in range(extraStudents):
-            _, c, p = heappop(ratios)
-            p +=1
-            c += 1
-            inc = (p+1)/(c+1) - p/c
-            heappush(ratios, (-inc,c,p))
+        i = 0
+        while h and i < extraStudents:
+            _, t, p = heappop(h)
+            t += 1
+            p += 1
+            inc = p/t - (p+1)/(t+1)
+            heappush(h, (inc, t,p))
+            i += 1
+        for _, t, p in h:
+            r += p / t
+        return r / len(classes)
 
-        return (sum(p/c for _,c,p in ratios) + perfect) / len(classes)
+        # 1/2
+        # 2/3 
 
-        
-        
+        # 2/4
+        # 3/5 = 0.6
