3db7dbfea12428fd91e8d4b4328800fe2f4dabd4
Fab <42979283+fchoi1@users.noreply.github.com>
2025-09-07 22:09:38 -0400

    Time: 0 ms (100%), Space: 17.7 MB (83.63%) - LeetHub

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        for i in range(1, n):
+            if '0' not in str(i) and '0' not in str(n - i):
+                return (i, n - i)
+        return (-1,-1)
