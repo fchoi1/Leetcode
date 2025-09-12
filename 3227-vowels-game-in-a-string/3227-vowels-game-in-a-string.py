51f93cf1535d8eb4d095964315d2ab0f2bf6ba0e
Fab <42979283+fchoi1@users.noreply.github.com>
2025-09-11 20:18:13 -0400

    Time: 55 ms (18.79%), Space: 17.9 MB (95.15%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
@@ -3,7 +3,7 @@ class Solution:
         vowels = 'aeiou'
         total = 0
         for char in s:
-            total += int(char in vowels)
+            total += (char in vowels)
             
         starts_ends_with_vowel = s[0] in vowels or s[-1] in vowels
         has_consonants = len(s) - total > 0
