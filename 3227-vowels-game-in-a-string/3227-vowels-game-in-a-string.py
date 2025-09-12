1c02326cc25e6b5d8f1fb67c6118a353132e4e5c
Fab <42979283+fchoi1@users.noreply.github.com>
2025-09-11 20:19:21 -0400

    Time: 52 ms (20.3%), Space: 18.2 MB (31.82%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
@@ -3,8 +3,7 @@ class Solution:
         vowels = 'aeiou'
         total = 0
         for char in s:
-            if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
-                total += 1
+            total += char in vowels
             
         if total == 0:
             return False
