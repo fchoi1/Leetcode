832429e43e65db5cbd0704747fec42fd3bb69f04
Fab <42979283+fchoi1@users.noreply.github.com>
2025-09-12 01:22:26 -0400

    Time: 0 ms (100%), Space: 17.7 MB (85.96%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,24 +1,70 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        prev = nums[-1]
-        for i, val in enumerate(nums[::-1]):
-            if prev > val:
-                break
-            prev = val
-        else:
-            nums = nums.reverse()
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        # conditions
+
+        # need end to find next?
+
+
+        #  5 1 (4 3) 2
+        #  5 1 4  2 3
+        #  5 1 2 4 3
+        #  5 2 1 4 3
+
+
+        #  5 3 6 7 2
+
+        # 5 3 7 2 6
+
+
+        #  5 3 9 7 2
+        #  5 7 2 3 9
+
+        #  5 3 9 7 3
+        #  5 7 3 3 9
+
+        #  5 2 1 3 4
+
+
+        #  1 3 5 4
+        #  1 4 3 5
+        def reverse(start):
+            end = len(nums) - 1
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        # get the index to swap with last when non acendubg'
+
+        index = len(nums) - 1
+
+        while index - 1 >= 0 and nums[index - 1] >= nums[index]:
+            index -= 1
+        index -= 1
+        if index == -1:
+            reverse(0)
             return
 
-        # First index to swap
-        index = len(nums)-i-1
+        # get the next smallest node
+        next_largest = len(nums) - 1
+        while next_largest - 1 >- 0 and nums[index] >= nums[next_largest]:
+            next_largest -= 1
+        nums[index], nums[next_largest] = nums[next_largest], nums[index]
+        reverse(index + 1)
 
-        # loop again and get next highests to swap within range
-        for i in range(len(nums)-1, index, -1):
-            if nums[i] > nums[index]:
-                break
 
-        # swap indexes
-        nums[index], nums[i] = nums[i], nums[index]
-        # reverse the sub array (no need to sort since it is already in descending order)
-        nums[index + 1:] = nums[index + 1:][::-1]
-        return
+
+
+
+        # 2 3 1 
+        # 3 1 2
+
+        # 3 1 2 1
+        # 3 2 1 1
+
+        # 4 2 3 1
+        # 
