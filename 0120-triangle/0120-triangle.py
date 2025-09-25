69aa2dc615e4815782c175e20de8204a91943f14
Fab <42979283+fchoi1@users.noreply.github.com>
2025-09-24 20:29:36 -0400

    Time: 20 ms (5.36%), Space: 21.4 MB (8.65%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,19 +1,25 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        prev = triangle[0]
-        minVal = prev[0]
-        for row in triangle[1:]:
-            temp = []
-            minVal = float('inf')
-            for i in range(len(row)):
-                if i == 0:
-                    val = prev[0] + row[i]
-                elif i >= len(prev):
-                    val = prev[-1] + row[i]
-                else:
-                    val = min(prev[i], prev[i-1]) + row[i]
-                minVal = min(minVal, val)
-                temp.append(val)
-            prev = temp
-            
-        return minVal
+
+        # dfs 
+        cache = {}
+        N = len(triangle)
+
+        # get min, sum at node
+        def dfs(index, row):
+            if (index, row) in cache:
+               return cache[(index,row)]
+
+            if row < N:
+                left = dfs(index, row + 1)
+                right = dfs(index + 1, row + 1)
+                
+                cache[(index, row)] = triangle[row][index] + min(left, right)
+                return cache[(index, row)]
+            else:
+                return 0
+
+    
+        
+        dfs(0,0)
+        return cache[(0,0)]
