4444e7ec632da1eb9535c606cf787688f7638245
Fab <42979283+fchoi1@users.noreply.github.com>
2025-10-25 00:35:43 -0400

    Time: 70 ms (77.13%), Space: 18.8 MB (5.85%) - LeetHub

---
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.py
@@ -0,0 +1,45 @@
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        
+
+        combos = []
+        num_str = str(n)
+        N = len(num_str)
+
+        def getCombos(i,val,targetLen):
+
+            if len(val) > targetLen:
+                return
+            elif len(val) == targetLen:
+                combos.append(val)
+                return 
+
+            
+            for idx in range(i, 7):
+                if idx + len(val) > targetLen:
+                    break
+                getCombos(idx + 1, val + str(idx) * idx, targetLen)
+
+        # Generate combos for length N only (sorted)
+        getCombos(1, '',N)
+        
+        minCombos = [1,22,122,1333,14444,122333,1224444]
+
+        valid = inf
+        for combo in combos:
+            if n >= int(combo[::-1]):
+                continue
+
+            # calculate perms of length N only
+            perms = [''.join(p) for p in permutations(combo)]
+
+            # get min valid 
+            for p in perms:
+                if int(p) > n:
+                    valid = min(valid, int(p))
+
+        # if no valid, get the next highest
+        if valid == inf:
+            return minCombos[N]
+            
+        return valid
