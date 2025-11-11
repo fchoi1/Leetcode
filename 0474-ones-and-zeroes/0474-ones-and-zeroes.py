0bd2de5bdadd28077ba8e1028d7986bb0f8f20e1
Fab <42979283+fchoi1@users.noreply.github.com>
2025-11-10 22:15:32 -0500

    Time: 2547 ms (15.76%), Space: 18 MB (95.64%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+        for s in strs:
+            zeros = s.count('0')
+            ones = len(s) - zeros
+            if zeros > m or ones > n:
+                continue
+
+            # get max in grid
+            max_count = 0
+            for y in range(n, ones - 1, -1):
+                for x in range(m, zeros - 1, -1):
+                    max_count = max(max_count, dp[y][x])
+                    dp[y][x] = max(dp[y][x], 1 + dp[y - ones][x - zeros])
+
+        return dp[-1][-1]
