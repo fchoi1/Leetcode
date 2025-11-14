076e3bd93e68e55bbbdc32ed4af793abd2dd7e16
Fab <42979283+fchoi1@users.noreply.github.com>
2025-11-14 01:03:01 -0500

    Time: 847 ms (20.19%), Space: 41.1 MB (18.31%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -1,14 +1,6 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        # store this data?
-        # return matrix
 
-
-        # track each of the four corners
-        # +1 for left bottom and left top
-        # -1 for right bottom and right top
-        queries.sort()
-        
         mat = [[0 for _ in range(n)] for _ in range(n)]
 
         for y1, x1, y2, x2 in queries:
@@ -17,14 +9,15 @@ class Solution:
                 if x2 + 1 < n:
                     mat[y][x2 + 1] -= 1
     
-        ans = [[0 for _ in range(n)] for _ in range(n)]
+        ans = []
         for y in range(n):
             curr = 0
+            row = []
             for x in range(n):
                 curr += mat[y][x]
-                ans[y][x] = curr
+                row.append(curr)
+            ans.append(row)
 
-    
         return ans
 
 
