a4b16f5ad0b0e38dc5eed14d08ffcaf14e92a9e1
Fab <42979283+fchoi1@users.noreply.github.com>
2025-12-18 22:14:56 -0500

    Time: 848 ms (7.65%), Space: 94.9 MB (5.1%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,49 +1,39 @@
-class Node:
-    def __init__(self,val):
-        self.val = val
-        self.neighbors = []
-
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        secret = set([0, firstPerson])
+        # combine all the linkages, particular order
+        # sort by time,
 
-        def dfs(node, seen):
-            if node in seen:
-                return
-            seen.add(node)
-            secret.add(node.val)
-            for nextNode in node.neighbors:
-                if not nextNode in seen:
-                    dfs(nextNode, seen)
+        secret = set()
 
-        def addSecret(sameTime):
-            nodes = {}
-            for x, y in sameTime:
-                if x not in nodes:
-                    nodes[x] = Node(x)
-                if y not in nodes:
-                    nodes[y] = Node(y)
-                nodes[x].neighbors.append(nodes[y])
-                nodes[y].neighbors.append(nodes[x])
+        # bfs
+        h = [(0,0), (0, firstPerson)] # time, person
 
-            seen = set()
-            for x,y in sameTime:
-                if x in seen or y in seen:
-                    continue
-                if x in secret or y in secret:
-                    dfs(nodes[x], seen)
-                
-        meetings.sort(key=lambda x: (x[2]))
-        prev = meetings[0][2]
-        sameTime = []
-        for x, y, time in meetings:
-            if time == prev:
-                sameTime.append([x, y])
-            else:
-                addSecret(sameTime)
-                prev = time
-                sameTime = [[x,y]]
-        addSecret(sameTime)
+        adjMap = defaultdict(dict)
+        for x,y,time in meetings:
+
+            if y not in adjMap[x]:
+                adjMap[y][x] = []
+                adjMap[x][y] = []
+      
+            adjMap[x][y].append(time)
+            adjMap[y][x].append(time)
+
+
+
+        while h:    # queue of people w/ secret
+            time, person = heapq.heappop(h)
+            if person in secret:
+                continue
+            secret.add(person)
+    
+
+            for nextPerson, meetingTimes in adjMap[person].items():
+                for meetingTime in meetingTimes:
+                    if meetingTime < time:
+                        continue
+                    heapq.heappush(h, (meetingTime, nextPerson))
+
+
         return list(secret)
 
-        
+
