dc71ee96a4409381f9647bbd049a350274e6a3aa
Fab <42979283+fchoi1@users.noreply.github.com>
2025-12-21 04:22:12 -0500

    Time: 15 ms (7.5%), Space: 17.9 MB (65.88%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -3,17 +3,15 @@ class Solution:
 
 
         parsedLogs = []
+        total = [0 for _ in range(n)]
+        stack = [] # fId, startTime
         for s in logs:
             fId, action, time = s.split(':')
             
             heappush(parsedLogs, (int(time), int(fId), action == 'end'))
         
-        total = [0 for _ in range(n)]
-        stack = [] # fId, startTime
-        print(parsedLogs)
         while len(parsedLogs) > 0:
 
-            print(stack, "log", parsedLogs[0], total)
             time, fId, isEnd = heappop(parsedLogs)
 
             if not isEnd:
@@ -25,7 +23,6 @@ class Solution:
                 # assumed the top of the stack
                 currId, currTime = stack.pop()
                 total[currId] += time - currTime + 1
-                print("popping", currId, currTime, time, total)
                 
                 if stack:
                     stack[-1] = (stack[-1][0], time + 1)
