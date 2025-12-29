4688cea7f8b60b87787b172308f3a6f2ef54fabf
Fab <42979283+fchoi1@users.noreply.github.com>
2025-12-28 22:44:29 -0500

    Time: 5712 ms (14.36%), Space: 18 MB (69.31%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
@@ -1,25 +1,16 @@
 class Solution:
     def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
         # backtrack
-        # dp?
-
         allowMap = defaultdict(list)
-        allowSet = set(allowed)
         for string in allowed:
-            bot =  string[:2]
-            top = string[2]
-            allowMap[bot].append(top)
+            allowMap[string[:2]].append(string[2])
 
-        N = len(bottom)
-
-        # bfs
-
-        def checkValid(row, currSet):
+        def checkValid(row):
 
             if len(row) == 1:
                 return True
             
-            nextRows = [('', currSet)] # (nextRow, nextSet)
+            nextRows = [''] # (nextRow, nextSet)
 
             # generate all rows?
             for a,b in zip(row, row[1:]):
@@ -32,20 +23,19 @@ class Solution:
                 
                 temp = []
                 for top in allowMap[bot]:
-                    for nextRow, nextSet in nextRows:
+                    for nextRow in nextRows:
                         nextRow += top
 
-                        temp.append((nextRow, nextSet & set(bot)))
+                        temp.append((nextRow))
 
                 nextRows = temp
 
 
-            for nextRow, nextSet in nextRows:
-                if checkValid(nextRow, nextSet):
+            for nextRow in nextRows:
+                if checkValid(nextRow):
                     return True
 
             return False
-                # get possible, construct all combos?
 
 
-        return checkValid(bottom, set())
+        return checkValid(bottom)
