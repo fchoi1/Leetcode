ef54a123910a15d574b9738f656df54ec8da49c0
Fab <42979283+fchoi1@users.noreply.github.com>
2025-12-29 22:56:13 -0500

    Time: 4 ms (12.12%), Space: 17.5 MB (87.27%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,31 +1,35 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-
-        def checkSquare(x,y):
-            seen = set()
-            target = sum(grid[y][x:x+3])
-            for i in range(3):
-                seen = seen.union(grid[y + i][x:x+3])
-                if sum(grid[y + i][x:x+3]) != target:
-                    return False
-                if sum(row[x + i] for row in grid[y:y+3]) != target:
-                    return False
-
-            diag1 = grid[y][x] + grid[y + 1][x + 1] + grid[y + 2][x + 2] != target
-            diag2 = grid[y+2][x] + grid[y + 1][x + 1] + grid[y][x + 2] != target
-            if  diag1 or diag2 or seen != {1,2,3,4,5,6,7,8,9}:
-                return  False
-            return True
-            
-        rows = len(grid)
-        cols = len(grid[0])
         
-        if rows < 3 or cols < 3:
-            return 0
 
-        count = 0
-        for y in range(rows-2):
-            for x in range(cols-2): 
-                if checkSquare(x,y):
-                    count += 1 
-        return count
+        def isMagic(startX, startY): # top left
+            numSet = set()
+            # diags
+            diag1 = grid[startY][startX] + grid[startY + 1][startX + 1] + grid[startY + 2][startX + 2] 
+            diag2 = grid[startY][startX + 2] + grid[startY + 1][startX + 1] + grid[startY + 2][startX] 
+            print("diag", startX, startY, diag1, diag2)
+            if diag1 != 15 or diag2 != 15:
+                return False
+
+            # row / col
+            for x in range(3):
+                if sum(row[startX + x] for row in grid[startY: startY + 3]) != 15:
+                    return False
+
+                for y in range(3):
+                    if sum(grid[y + startY][startX: startX + 3]) != 15:
+                        return False
+                    numSet.add(grid[y + startY][x + startX])
+                    
+            print(numSet, "cstart", startX, startY)
+            return numSet == {1,2,3,4,5,6,7,8,9}
+        
+
+        H = len(grid)
+        W = len(grid[0])
+        ans = 0
+        for y in range(0, H - 2):
+            for x in range(0, W - 2):
+                if isMagic(x,y):
+                    ans += 1
+        return ans
