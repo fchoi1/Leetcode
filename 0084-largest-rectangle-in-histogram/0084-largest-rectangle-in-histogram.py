c09dabbdded59640a84e4846b2e4de8eaad24d7a
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-09 23:24:31 -0500

    Time: 121 ms (95.36%), Space: 38.2 MB (5.22%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,18 +1,30 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        stack = [] # tuple hieght and index
-        maxArea = 0
-        for i,h in enumerate(heights):
-            if stack and h < stack[-1][0]:
-                while stack and h < stack[-1][0]:
-                    height, index = stack.pop()
-                    width = i - index
-                    maxArea = max(maxArea, height * width)
-                stack.append((h, index))
-            else:
-                stack.append((h,i))
-        while stack:
-            height, index = stack.pop()
-            width = len(heights) - index
-            maxArea = max(maxArea, height * width)
-        return maxArea
+        
+
+        # 3 2 1 2 3
+
+        # track lowests and things above it
+
+        # pop out things in the stack when lower
+        largest = 0
+        stack = [] # height, idx
+
+
+        for i, h in enumerate(heights):
+            idx = i
+            while stack and stack[-1][0] > h:
+                height, idx, _= stack.pop()
+                largest = max(largest, height * (i - idx))
+            stack.append((h,idx,i))
+
+        if not stack:
+            return largest
+        
+        N = stack[-1][2]
+        # print(stack, largest, N)
+        for h,idx,i in stack:
+            # print(h, N-idx+1, i, idx)
+            largest = max(largest, (N - idx + 1) * h, h * (i - idx + 1))
+
+        return largest
