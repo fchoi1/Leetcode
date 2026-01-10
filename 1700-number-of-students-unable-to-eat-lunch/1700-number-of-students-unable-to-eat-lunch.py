67ddda51e63b66e7dda2014a866b9e109232f778
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-10 12:16:08 -0500

    Time: 0 ms (100%), Space: 19.5 MB (5.46%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,12 +1,10 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        count_students = Counter(students)
-        
-        while sandwiches and count_students[0] > 0 and count_students[1] > 0:
-            count_students[sandwiches.pop(0)] -= 1
+        counts = Counter(students)
 
-        val = 0 if count_students[0] > 0 else 1
-
-        while sandwiches and sandwiches[0] == val:
-            sandwiches.pop(0)
-        return len(sandwiches)
+        for s in sandwiches:
+            if counts[s] != 0:
+                counts[s] -= 1
+            else:
+                return sum(counts.values())
+        return 0
