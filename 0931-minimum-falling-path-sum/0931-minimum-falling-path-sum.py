a745834091bda16279fc7649be6a43a6f626b496
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-11 22:51:01 -0500

    Time: 73 ms (9.01%), Space: 27.2 MB (13.78%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,40 +1,27 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        # dp?
-        # bottom up dp store the best per each row look below (x-1, x, x + 1) - borders
+        # dfs 
 
-
-        # 2 1 3                                             -> (2 + 12) (1 + 12) (2 + 13) ->  14 13 15
-        # 6 5 4 -> (6 + 7), (5 + 7),  (4 + 8) -> 13 12 12
-        # 7 8 9 ->                            -> 7 8 9
-
-
-        N = len(matrix)
+        H = len(matrix)
         W = len(matrix[0])
 
-        # start from bottom
-
-        prev = [] # current row
-
-        for i in range(N - 1, -1, -1):
-            if i == N - 1:
-                prev = matrix[i]
-                continue
-            curr = []
-            for x in range(W):
-                lowest = prev[x]
-
-                if x - 1 >= 0:
-                    lowest = min(lowest, prev[x-1])
-                
-                if x + 1 < W:
-                    lowest = min(lowest, prev[x+1])
-
-                curr.append(lowest + matrix[i][x])
+        @cache
+        def dfs(row, col):
             
-            prev = curr
+            # base case
+            if row == H-1:
+                return matrix[row][col]
+            minSum = inf
+            for i in range(-1,2):
+                if col + i >= 0 and col + i < W:
+                    minSum = min(minSum,dfs(row + 1, col + i))
+            return minSum + matrix[row][col]
         
-        return min(prev)
-                
+        ans = inf
+        for c in range(W):
+            ans = min(ans, dfs(0, c))
 
+        return ans
 
+            
+        
