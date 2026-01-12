85f5990f0b1c5b7a4840aab6583c72937e3aa3e0
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-12 18:42:06 -0500

    Time: 0 ms (100%), Space: 19.4 MB (5.66%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -5,11 +5,6 @@ class Solution:
         for prev, curr in zip(points, points[1:]):
             px, py = prev
             cx, cy = curr
-
-            maxDiff = max(abs(px - cx), abs(py - cy))
-            minDiff = min(abs(px - cx), abs(py - cy))
-            diff = maxDiff - minDiff
-
-            time += diff + minDiff
+            time +=  max(abs(px - cx), abs(py - cy))
 
         return time
