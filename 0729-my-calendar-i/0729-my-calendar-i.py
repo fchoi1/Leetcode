26b1080d44ec39a58373d50fb4f3c882eebd64f0
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-13 23:14:24 -0500

    Time: 313 ms (19.07%), Space: 17.3 MB (100%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,35 +1,14 @@
 class MyCalendar:
-
     def __init__(self):
-        self.heap = []
-        
+        self.eventDates = []
+
     def book(self, start: int, end: int) -> bool:
-        if not self.heap or end <= self.heap[0][0]:
-            heappush(self.heap, (start,end))
-            return True
+        for laststart,lastend in self.eventDates:
+            if laststart < end and start < lastend: # means there is overlapping
+                return False # so we can return False
+        self.eventDates.append((start,end))
+        return True
 
-        temp = []
-        currTime = nextTime = None
-        noOverlap = False
-    
-        while self.heap and start >= self.heap[0][1]:
-            currTime = heappop(self.heap)
-            heappush(temp, currTime)
-
-        if not currTime:
-            currTime = heappop(self.heap)
-            heappush(temp, currTime)
-
-        if self.heap:
-            if start >= currTime[1] and end <= self.heap[0][0]:
-                heappush(temp, (start,end))
-                noOverlap = True
-        else:
-            if start >= currTime[1]:
-                heappush(temp, (start,end))
-                noOverlap = True
-
-        while self.heap:
-            heappush(temp, heappop(self.heap))
-        self.heap = temp
-        return noOverlap
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
