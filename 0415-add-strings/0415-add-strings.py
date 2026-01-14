c46095cf7859f6047641232eb51b234e45a8cebd
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-13 23:32:15 -0500

    Time: 3 ms (88.77%), Space: 19.4 MB (11.29%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,22 +1,25 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+
         num1 = num1[::-1]
         num2 = num2[::-1]
-        n1 = n2 = carryOver = 0
-        numStr = ""
-        while n1 < len(num1) or n2 < len(num2) or carryOver :
-            val = carryOver
-            if n1 < len(num1):
-                val += int(num1[n1])
-                n1 += 1
-                
-            if n2 < len(num2):
-                val += int(num2[n2])
-                n2 += 1
 
-            carryOver = int(val > 9)
-            numStr += str(val % 10)
+        carry =  i = j = 0
+        ans = ""
+        while i < len(num1) or j < len(num2):
+            
+            n1 = int(num1[i]) if i < len(num1) else 0
+            n2 = int(num2[i]) if j < len(num2) else 0
+            total = n1 + n2 + carry
+            val = (total) % 10 
+            carry = (total) // 10
 
-        return numStr[::-1]
+            ans = str(val) + ans
+            i += 1
+            j += 1
 
-        
+
+        if carry:
+            ans = "1" + ans
+            
+        return ans
