852e00b7658c9c6b7c0258396a637efec480daba
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-13 23:24:40 -0500

    Time: 4 ms (59.73%), Space: 20.6 MB (9.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,14 +1,15 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        fast = 0
-        for slow, n in enumerate(nums):
-            if fast < slow:
-                fast = slow
-            while nums[fast] == 0:
-                fast += 1
-                if fast == len(nums):
-                    return
-            if n == 0:
-                nums[slow], nums[fast] = nums[fast],0
-
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         
+        left = 0
+
+        for i in range(len(nums)):
+
+            if nums[i] != 0:
+                nums[left] = nums[i]
+                if left != i:
+                    nums[i] = 0
+                left += 1
