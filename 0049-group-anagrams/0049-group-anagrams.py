79dead54d8a1afbf203928749817642ff1d044a8
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-14 23:09:40 -0500

    Time: 23 ms (18.92%), Space: 24.1 MB (5.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # hash
-        d = defaultdict(list)
-        ans = []
-        for word in strs:
-            t = [0] * 26
-            for char in word:
-                t[ord(char)-ord('a')] += 1
-            d[",".join(str(n) for n in t)].append(word)
-        
-        return [v for v in d.values()]
-            
+        def getHash(string):
+            t = [0 for _ in range(26)]
+            for char in string:
+                t[ord(char) - ord('a')] += 1
+            return tuple(t)
+
+        anagrams = defaultdict(list)
+        for s in strs:
+            h = getHash(s)
+            anagrams[h].append(s)
+        return list(anagrams.values())
