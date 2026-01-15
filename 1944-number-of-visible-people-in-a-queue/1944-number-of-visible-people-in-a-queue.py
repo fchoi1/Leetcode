9a3ae1800eb330166a49fca19a421b93b2afb614
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-15 00:19:49 -0500

    Time: 123 ms (9.78%), Space: 35.7 MB (5.35%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -1,26 +1,20 @@
 class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
 
-        # forwards then backwards pass
-
-        # start from lower - increasing
-
         N = len(heights)
         ans = [0 for _ in range(N)]
         stack = [] # height, index        
 
         for i, h in enumerate(heights):
-            currPop = 0
             while stack and h > stack[-1][0]:
                 _, idx = stack.pop()
-                currPop += 1
                 ans[idx] += 1
                 if stack:
                     ans[stack[-1][1]] += 1
                 
             stack.append((h,i))
             
-        for i, (_, idx) in enumerate(stack[:-1]):
+        for _, idx in stack[:-1]:
             ans[idx] += 1
 
         return ans
