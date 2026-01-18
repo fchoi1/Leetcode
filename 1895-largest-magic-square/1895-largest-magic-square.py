3cf8d8238170fb5d7a137c78c2f6ff30fa134d6e
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-17 21:11:53 -0500

    Time: 4751 ms (8.05%), Space: 19.7 MB (12.07%) - LeetHub

---
+++ 1895-largest-magic-square/1895-largest-magic-square.py
@@ -0,0 +1,43 @@
+class Solution:
+    def largestMagicSquare(self, grid: List[List[int]]) -> int:
+        
+
+        def isMagic(x,y,length):
+            target = sum(grid[y][dx] for dx in range(x, x + length))
+            
+            # diag
+            diag1 = sum(grid[y + i][x + i] for i in range(length))
+            diag2 = sum(grid[y + length - i - 1][x + i] for i in range(length))
+            if diag1 != target:
+                return False
+            if diag2 != target:
+                return False
+            
+            for i in range(length):
+                # rows
+                row = sum(grid[y + i][dx] for dx in range(x, x + length))
+                if row != target:
+                    return False
+
+                # cols
+                col = sum(grid[dy][x + i] for dy in range(y, y + length))
+                if col != target:
+                    return False
+            return True
+
+        W = len(grid[0])
+        H = len(grid)
+
+        maxK = 1
+        
+        for n in range(1, min(W, H) + 1):
+            for y in range(H - n + 1):
+                for x in range(W - n + 1):
+                    if isMagic(x,y,n):
+                        maxK = n
+            #             break
+            #     else:
+            #         break
+            # else:
+            #     break
+        return maxK
