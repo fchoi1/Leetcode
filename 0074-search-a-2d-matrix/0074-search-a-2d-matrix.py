e82f04e61e704366e3f06ba2cae7c7cf03891298
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-19 23:32:54 -0500

    Time: 0 ms (100%), Space: 19.8 MB (9.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,39 +1,38 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        # search  row
+        # check row
 
-        # search col
 
-        mid = l = 0
+        l = 0
         r = len(matrix) - 1
+
+        # 0, 2
         while l < r:
             mid = (l + r) // 2
-            print(mid, r, l, target, matrix[mid])
-            if matrix[mid][0] == target:
-                return True
-            
+
             if matrix[mid][0] <= target <= matrix[mid][-1]:
                 l = mid
                 break
-            
-            if target > matrix[mid][0]:
-                l = mid + 1
-            else:
+            elif target < matrix[mid][0]:
                 r = mid - 1
-            print('chec', l, mid, r, target, matrix[mid][0])
-     
-        
-        row = matrix[l]
-        l = 0
-        r = len(row)
-        print("brok", row, l ,r, mid)
-        while l < r:
-            mid = (l + r) //  2
-            if row[mid] == target:
-                return True
-            if target > row[mid]:
-                l = mid + 1
             else:
-                r = mid 
-        return False
+                l = mid + 1
+
+        row = matrix[l]
+
+
+        l = 0
+        r = len(row) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if target == row[mid]:
+                return True
+            elif row[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return row[min(l,r)] == target
