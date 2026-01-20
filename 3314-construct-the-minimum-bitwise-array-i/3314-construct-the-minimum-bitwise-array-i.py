b4a12772f81c7154b2170b949bc19910913298c7
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-19 22:55:36 -0500

    Time: 14 ms (65.44%), Space: 19.4 MB (15.44%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
@@ -2,14 +2,15 @@ class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
         ans = []
 
-        # prime it always odd
-        # 
-        cache = {}
 
-        for x in range(max(nums)):
+        cache = {}
+        N = max(nums)
+
+        for x in range(N):
             val = x | x + 1
             if val not in cache:
                 cache[x | x + 1] = x
+
         for n in nums:
             if n not in cache:
                 ans.append(-1)
