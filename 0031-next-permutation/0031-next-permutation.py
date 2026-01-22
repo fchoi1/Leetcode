416796cf10295b260029ba744145d472aa5573a2
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-21 23:39:49 -0500

    Time: 1 ms (9.82%), Space: 19.5 MB (8.16%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,68 +3,48 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        # conditions
 
-        # need end to find next?
-
-
-        #  5 1 (4 3) 2
-        #  5 1 4  2 3
-        #  5 1 2 4 3
-        #  5 2 1 4 3
+        # 1 2 3 
+        # 1 3 2
 
 
-        #  5 3 6 7 2
-
-        # 5 3 7 2 6
-
-
-        #  5 3 9 7 2
-        #  5 7 2 3 9
-
-        #  5 3 9 7 3
-        #  5 7 3 3 9
-
-        #  5 2 1 3 4
-
-
-        #  1 3 5 4
-        #  1 4 3 5
-        def reverse(start):
-            end = len(nums) - 1
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-
-        # get the index to swap with last when non acendubg'
-
-        index = len(nums) - 1
-
-        while index - 1 >= 0 and nums[index - 1] >= nums[index]:
-            index -= 1
-        index -= 1
-        if index == -1:
-            reverse(0)
-            return
-
-        # get the next smallest node
-        next_largest = len(nums) - 1
-        while next_largest - 1 >- 0 and nums[index] >= nums[next_largest]:
-            next_largest -= 1
-        nums[index], nums[next_largest] = nums[next_largest], nums[index]
-        reverse(index + 1)
-
-
-
-
-
-        # 2 3 1 
-        # 3 1 2
-
-        # 3 1 2 1
-        # 3 2 1 1
-
-        # 4 2 3 1
+        # go for the end until descending, switch end
         # 
+
+
+        # 3 2 1 -> needs full reverse
+
+        # 3 1 4 2 -> go until 2 > 1, no need to reverse? we do?
+        # 3 2 1 4
+
+
+        # 3 1 5 4 2
+
+        # 3 2 1 4 5
+
+        # 3 1 4 5 2
+        # 3 1 5 2 4
+
+        N = len(nums) - 1
+        swap = -1
+        for i in range(N, 0, -1):
+            if nums[i - 1] < nums[i]:
+                # found the switch 
+                swap1 = i - 1
+                break
+        else:
+            nums.reverse()
+            return
+            
+        for i in range(N, swap1, -1):
+            if nums[swap1] < nums[i]:
+                swap2 = i
+                break
+        else:
+            print("Logic error?")
+
+        nums[swap1], nums[swap2] = nums[swap2], nums[swap1]
+        print("Swap", swap1, swap2, nums,  nums[swap1::-1])
+
+        nums[swap1+1:] = nums[swap1+1:][::-1]
+        print(nums)
