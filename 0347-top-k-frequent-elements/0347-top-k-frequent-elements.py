273fd0f0ce094af9ddacb003dd70f13658858e4e
Fab <42979283+fchoi1@users.noreply.github.com>
2026-01-24 23:05:01 -0500

    Time: 0 ms (100%), Space: 22.9 MB (31.29%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,4 +1,5 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:   
-        return [n[0] for n in sorted(Counter(nums).items(), key=lambda x:x[1], reverse=True)[:k]]
-        
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+            counts = [(-v,k) for k,v in Counter(nums).items()] 
+            heapify(counts)
+            return [heappop(counts)[1] for _ in range(k)]
