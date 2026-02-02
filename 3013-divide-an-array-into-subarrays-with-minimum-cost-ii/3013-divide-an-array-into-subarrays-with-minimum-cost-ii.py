85e7f3aebf94f40b22158119875670df418c0bde
Fab <42979283+fchoi1@users.noreply.github.com>
2026-02-01 22:19:30 -0500

    Time: 677 ms (100%), Space: 45.1 MB (8.7%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -1,16 +1,13 @@
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        # sliding window
+        # sliding window + heap
 
-        # heap
-      
-        
-        # min cost heap length k
         maxHeap = []
         minHeap = []
         small = set()
 
-        c = 0
+
+        # construct heaps
         for i in range(1,dist + 2):
             heappush(maxHeap, (-nums[i], i))
             small.add(i)
@@ -18,7 +15,6 @@ class Solution:
                 val, idx = heappop(maxHeap)
                 heappush(minHeap, (-val, idx))
                 small.remove(idx)
-            c += 1
 
         curr = sum(-h[0] for h in maxHeap)
         minCost = curr
@@ -27,7 +23,8 @@ class Solution:
         for n in nums[dist + 2:]:
             
             removeIdx = idx - dist - 1
-
+            
+            # remove stale idx
             while maxHeap and (maxHeap[0][1] <= removeIdx):
                 heappop(maxHeap)
 
@@ -37,38 +34,31 @@ class Solution:
             # push to minHeap first
             heappush(minHeap, (n, idx))
 
+            # If we removed an index in small
             if removeIdx in small:
                 curr -= nums[removeIdx]
                 small.remove(removeIdx)
 
-    
-            # remove from maxHeap
-
-            while (len(small) < k - 1 and minHeap):
+                sVal, sId = heappop(minHeap)
+                heappush(maxHeap, (-sVal, sId))
+                small.add(sId)
+                curr += sVal
                 
-             
-                smallest, sId = heappop(minHeap)
-                heappush(maxHeap, (-smallest, sId))
+            # swap min and max heap vals
+            if minHeap and maxHeap and minHeap[0][0] <= -maxHeap[0][0]:
+                rVal, rId = heappop(maxHeap)
+                sVal, sId = heappop(minHeap)
+
+                small.remove(rId)
+                curr += rVal
 
                 small.add(sId)
-                curr += smallest
-            
-            if minHeap and maxHeap:
-                # swap min and max heap vals
-                if minHeap[0][0] <= -maxHeap[0][0]:
-                    rVal, rId = heappop(maxHeap)
-                    small.remove(rId)
-                    curr += rVal
+                curr += sVal
 
-                    smallest, sId = heappop(minHeap)
-                    small.add(sId)
-                    curr += smallest
-
-                    heappush(maxHeap, (-smallest, sId))
-                    heappush(minHeap, (-rVal, rId))
+                heappush(maxHeap, (-sVal, sId))
+                heappush(minHeap, (-rVal, rId))
 
             minCost = min(minCost, curr)
             idx += 1
                 
-
         return nums[0] + minCost
