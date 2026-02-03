39a00ebc6faec6cadf39b4150372d2024f9efff1
Fab <42979283+fchoi1@users.noreply.github.com>
2026-02-02 22:37:06 -0500

    3952-TRIONIC-ARRAY-I Stats: Time: 4 ms (84.16%), Space: 19.4 MB (19.11%) - LeetHub

--- 3952-trionic-array-i/3952-trionic-array-i.py
+++ 3952-trionic-array-i/3952-trionic-array-i.py
@@ -1,25 +1,20 @@
 class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
-        # up down up
-
-        if len(nums) == 3 or nums[1] < nums[0]:
+        
+        # impossible if lenghth is 3 or starts off decreasing
+        if len(nums) <= 3 or nums[1] < nums[0]:
             return False
 
-        switch = 0
-        increase = True
+        switch, increase = 0, True
 
         for prev, curr in zip(nums, nums[1:]):
+            # Can't be equal
             if prev == curr:
                 return False
             
-            if increase and curr < prev:
-                increase = False
+            # count how many times it switches
+            if increase != (curr > prev):
+                increase = not increase
                 switch += 1
-            elif not increase and curr > prev:
-                increase = True
-                switch += 1
-            
-            print(increase, prev, curr, switch)
-            
-        print(switch, increase)
+
         return switch == 2
