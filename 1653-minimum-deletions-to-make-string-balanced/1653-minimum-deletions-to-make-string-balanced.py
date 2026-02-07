438a63d8bf158a1ba698582d035d116f9043b983
Fab <42979283+fchoi1@users.noreply.github.com>
2026-02-06 23:51:53 -0500

    Time: 273 ms (53.97%), Space: 20.1 MB (55.07%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,19 +1,14 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         b = a = 0
-        a_count = []
-        b_count = []
         a_total = s.count("a")
+        min_delete = math.inf
+
         for c in s:
-            b_count.append(b)
             if c == 'b':
+                min_delete = min(min_delete, b + a_total - a)
                 b += 1
             else:
                 a += 1
-            a_count.append(a_total - a)
-        
-        min_change = inf
-        for left, right in zip(b_count, a_count):
-            if left + right < min_change:
-                min_change = left + right
-        return min_change
+                min_delete = min(min_delete, b + a_total - a)
+        return min_delete
