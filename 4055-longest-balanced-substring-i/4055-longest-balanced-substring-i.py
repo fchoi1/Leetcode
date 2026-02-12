e5e72c93d55e167a677cb46e8f4d0074548c384b
Fab <42979283+fchoi1@users.noreply.github.com>
2026-02-11 21:31:22 -0500

    4055-LONGEST-BALANCED-SUBSTRING-I Stats: Time: 2589 ms (62.2%), Space: 19.4 MB (20.27%) - LeetHub

--- 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.py
+++ 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.py
@@ -4,24 +4,20 @@ class Solution:
         # abbbbba
         # brute force + greedy?
 
-        N = len(s)
-
-        # start at lenght N to 0
-        
+        N = len(s)        
         for currLen in range(N, -1, -1):
             counts = Counter(s[:currLen])
-            print("curr", currLen, counts)
-            if len(set(counts.values())) <= 1:
-                print('done', counts)
-                return currLen
+  
+            for i in range(N - currLen + 1):
+                if len(set(counts.values())) <= 1:
+                    return currLen
+                
+                if i + currLen >= N:
+                    break
 
-            for i in range(N - currLen):
                 counts[s[i]] -= 1
                 if counts[s[i]] == 0:
                     del counts[s[i]]
                 counts[s[i + currLen]] += 1
                 
-                if len(set(counts.values())) <= 1:
-                    print('done', counts, "loop", i)
-                    return currLen
         return 0
