add503a2bb7219d21ef989466ae8d05a39bcab22
Fab <42979283+fchoi1@users.noreply.github.com>
2026-02-14 19:19:48 -0500

    Time: 11 ms (5.47%), Space: 19.6 MB (12.44%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,3 +1,27 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2) + int(b,2))[2:]
+        A, B = len(a), len(b)
+        i, j, carry = 0,0,0
+        a = a[::-1]
+        b = b[::-1]
+        ans = ''
+
+        while i < A or j < B:
+            n1 = 0 if i >= A else int(a[i])
+            n2 = 0 if j >= B else int(b[j])
+
+            total = n1 + n2 + carry
+           
+            carry = total // 2
+            val = total % 2
+            print(n1, n2, carry, val)
+
+            ans = str(val) + ans
+
+            i += 1
+            j += 1
+
+        if carry:
+            ans = str(carry) + ans
+        
+        return ans
