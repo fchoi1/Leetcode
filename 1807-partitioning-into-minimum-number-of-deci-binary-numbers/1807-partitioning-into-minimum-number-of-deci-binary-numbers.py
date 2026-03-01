46e8e8f0f1cf1bb1d39c247d4bd61828e4cefe94
Fab <42979283+fchoi1@users.noreply.github.com>
2026-02-28 22:03:59 -0500

    1807-PARTITIONING-INTO-MINIMUM-NUMBER-OF-DECI-BINARY-NUMBERS Stats: Time: 0 ms (100%), Space: 20 MB (61.18%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,3 +1,8 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        return int(max(n))
+        for c in '9876543210':
+            if c in n:
+                return int(c)
+        
+        return 0
+        
