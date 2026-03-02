8ae0a4e62fec6c4d0bec7104df03304efae6c86f
Fab <42979283+fchoi1@users.noreply.github.com>
2026-03-01 23:44:29 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.py
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.py
@@ -1,12 +1,10 @@
 class Solution:
     def minSwaps(self, grid: List[List[int]]) -> int:
-        # bubble sorting
         
         N = len(grid[0])
-        counts = defaultdict(int)
-        idxList = list(range(N))
         arr = []
- 
+
+        # get right most 1
         for idx, row in enumerate(grid):
             i = 0
             while i < N and row[N-i-1] == 0:
@@ -19,15 +17,16 @@ class Solution:
                 return -1
 
 
-        swap = 0
-        idx = 0
-
+        swap = idx = 0
+        
+        # simulate swaps
         for idx in range(N):
-            # valid
+
+            # idx already valid
             if arr[idx] <= idx:
                 continue
             
-            # find closes valid
+            # find next closes valid
             target = -1
             for i, val in enumerate(arr[idx:]):
                 if val <= idx:
@@ -37,11 +36,10 @@ class Solution:
                 print("ERR")
                 return -1
             
-            # swap += (target - idx)
+            swap += (target - idx)
             for i in range(target, idx, -1):
                 arr[i], arr[i-1] = arr[i-1], arr[i]
-                swap += 1
-                
+
         return swap
             
             
