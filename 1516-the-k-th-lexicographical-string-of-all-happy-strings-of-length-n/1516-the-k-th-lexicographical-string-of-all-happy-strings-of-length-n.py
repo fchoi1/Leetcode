a6c23d0e6fe39dc45851f5af00c24dcbe8eff5ce
Fab <42979283+fchoi1@users.noreply.github.com>
2026-03-14 00:38:40 -0400

    1516-THE-K-TH-LEXICOGRAPHICAL-STRING-OF-ALL-HAPPY-STRINGS-OF-LENGTH-N Stats: Time: 0 ms (100%), Space: 19.6 MB (63.47%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -2,48 +2,24 @@ class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         
 
-        # recurse into 3
+        self.ans = ""
+        self.count = 0
 
+        # generate happy strings
+        def backtrack(s):
+            if len(s) == n:
+                self.count += 1
+                if self.count == k:
+                    self.ans = s
+                    return True
+                return False
 
-        # 1 3                   3
-        # 2  ab ac ba bc ca cb  6
-        #                       12
-        # abab abac abca abcb acab acac acba acbc 
-
-        total = 3 * 2 ** (n-1)
+            for char in "abc":
+                if s and s[-1] == char:
+                    continue
+                if backtrack(s + char):
+                    return True
+            return False
         
-        if k > total:
-            return ''
-        if k <= total // 3:
-            curr = 'a'
-            l = 0
-            r = total // 3
-        elif total // 3 < k <= total // 3 * 2:
-            curr = 'b'
-            l = total // 3 + 1
-            r = total // 3 * 2
-        else:
-            curr = 'c'
-            l = total // 3 * 2 + 1
-            r = total
-
-        mapping = {
-            'a': ('b','c'),
-            'b': ('a','c'),
-            'c': ('a','b')
-        }
-
-        string = curr
-        for _ in range(n-1):
-            mid = (l + r) // 2
-            if k <= mid:
-                curr = mapping[curr][0]
-                r = mid
-            else:
-                curr = mapping[curr][1]
-                l = mid
-            string += curr
-
-        return string
-
-        
+        backtrack("")
+        return self.ans
