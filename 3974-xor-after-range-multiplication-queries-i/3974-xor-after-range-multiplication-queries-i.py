d318cfbbd95400f65652fd64d659d5cc9d1432a8
Fab <42979283+fchoi1@users.noreply.github.com>
2026-04-07 22:18:40 -0400

    3974-XOR-AFTER-RANGE-MULTIPLICATION-QUERIES-I Stats: Time: 1672 ms (37.78%), Space: 20.2 MB (48.61%) - LeetHub

--- 3974-xor-after-range-multiplication-queries-i/3974-xor-after-range-multiplication-queries-i.py
+++ 3974-xor-after-range-multiplication-queries-i/3974-xor-after-range-multiplication-queries-i.py
@@ -1,12 +1,14 @@
+from functools import reduce
+
 class Solution:
     def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
         mod = 10 ** 9 + 7
-        for l,r,k,v in queries:
-            while l <= r:
-                nums[l] = nums[l] * v % mod
-                l += k
 
-        val = nums[0]
-        for n in nums[1:]:
-            val ^= n
-        return val
+        for l, r, k, v in  queries:
+            idx = l
+            while idx <= r:
+                nums[idx] = (nums[idx] * v) % mod
+                idx += k
+        
+        return reduce(lambda prev, curr: prev ^ curr, nums)
+ 
