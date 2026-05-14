abec2c10f4501f0e3ad55d7caeb0c82f759bc93b
Fab <42979283+fchoi1@users.noreply.github.com>
2026-05-13 22:27:16 -0400

    Time: 3 ms (29.89%), Space: 19.2 MB (59.43%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,5 +1,11 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        c = Counter(nums)
         N = len(nums) - 1
-        return len(c) == N and c[N] == 2 and sum(nums) == N * (N + 1) // 2 + N
+        c = defaultdict(int)
+        curr = 0
+
+        for n in nums:
+            c[n] += 1
+            curr += n
+            
+        return len(c) == N and c[N] == 2 and curr == N * (N + 1) // 2 + N
