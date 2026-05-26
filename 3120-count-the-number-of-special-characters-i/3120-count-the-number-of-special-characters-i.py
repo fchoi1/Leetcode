d8a1e862a91cebeb38290f59670c47ea7ed8a185
Fab <42979283+fchoi1@users.noreply.github.com>
2026-05-25 21:16:16 -0400

    Time: 3 ms (37.34%), Space: 19.2 MB (91.98%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,15 +1,6 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        upper = set()
-        lower = set()
-        seen = set()
-        for char in word:
-            if char == char.upper():
-                if char.lower() in lower:
-                    seen.add(char.lower())
-                upper.add(char)
-            else:
-                if char.upper() in upper:
-                    seen.add(char)
-                lower.add(char)
-        return len(seen)
+        w = set(word)
+        
+        return sum( x.upper() in word and x in word for x in  list(string.ascii_lowercase))
+ 
