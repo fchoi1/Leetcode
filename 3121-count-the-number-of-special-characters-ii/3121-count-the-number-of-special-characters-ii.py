6cc028c7ed6b5dddd69f1140ac8d219b3c0b6636
Fab <42979283+fchoi1@users.noreply.github.com>
2026-05-26 23:42:47 -0400

    Time: 362 ms (25.3%), Space: 21.5 MB (62.94%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,19 +1,14 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        c = defaultdict(int)
-        ans = [None] * 26
-        for char in word:
-            key = ord(char.lower()) - ord('a')
-            c[char] += 1
-
-            if ans[key] is None:
-                if c[char.upper()] > 0 and c[char.lower()] > 0:
-                    ans[key] = True
-
-            if char.lower() == char:
-                if c[char.upper()] > 0:
-                    ans[key] = False
-
+        charSet = set()
+        seen = set()
         
-        return sum(v if v else 0 for v in ans)
-
+        for char in word:
+            if char == char.upper():
+                if char.lower() in charSet and char not in charSet:
+                    seen.add(char.lower())
+            else:
+                if char.upper() in charSet:
+                    seen.discard(char)
+            charSet.add(char)
+        return len(seen)
