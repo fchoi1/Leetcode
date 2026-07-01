beaffdb8f087293e9faf6d99754baf7f88afcc20
Fab <42979283+fchoi1@users.noreply.github.com>
2026-06-30 20:56:54 -0400

    Time: 2636 ms (86.71%), Space: 55.7 MB (38.15%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -1,60 +1,49 @@
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
-        N =  len(grid)
-        self.safe = [[inf for _ in range(N)] for _ in range(N)]   
-    
-        def traverse(x ,y):
-            q = deque([(x,y)])
-            seen = set()
-            step = 0
-            while q:
-                for _ in range(len(q)):
-                    x,y = q.popleft()
-                    if (x,y) in seen or step >= self.safe[y][x]:
-                        continue
-                    seen.add((x,y))
-                    self.safe[y][x] = step
-                                        
-                    for dx, dy in [(0,1), (1,0), (-1,0), (0, -1)]:
-                        if 0 <= x + dx < N and 0 <= y + dy < N and grid[y + dy][x + dx] != 1 and (x + dx, y + dy) not in seen:
-                            q.append((x + dx, y + dy))
-                step += 1
 
-        seen = set()
-       
-        # for y in range(N):
-        #     for x in range(N):
-        #         if grid[y][x]:
-        #             traverse(x, y)
-    
-        self.safe = [[inf] * N for _ in range(N)]
-        for i in range(N):
-            for j in range(N):
-                if grid[i][j] == 1:
-                    self.safe[i][j] = 0
+        W = len(grid[0])
+        H = len(grid)
+
+        costs = [ [inf for _ in range(W)] for _ in range(H)]
+        for y in range(H):
+            for x in range(W):
+                if grid[y][x]:
+                    costs[y][x] = 0
+                    continue
+                elif not x and not y:
+                    continue
+                elif not y:
+                    costs[y][x] = 1 + costs[y-1][x]
+                elif not x:
+                    costs[y][x] = 1 + costs[y][x-1]
                 else:
-                    self.safe[i][j] = 1 + min(self.safe[i-1][j] if i - 1 >= 0 else inf, 
-                                    self.safe[i][j-1] if j - 1 >= 0 else inf)
-        for i in range(N-1, -1, -1):
-            for j in range(N-1, -1, -1):
-                if grid[i][j] == 0:
-                    self.safe[i][j] = min(self.safe[i][j],
-                        (1 + self.safe[i+1][j]) if i + 1 < N else inf, 
-                        (1 + self.safe[i][j+1]) if j + 1 < N else inf)
-        # dfs
-        q = [[-self.safe[0][0],0,0]]
+                    costs[y][x] = 1 + min(costs[y][x-1], costs[y-1][x])
 
+        for y in range(H-1,-1,-1):
+            for x in range(W-1,-1,-1):
+                if x + 1 >= W and y + 1 >= H:
+                    continue
+                elif x + 1 >= W:
+                    costs[y][x] = min(costs[y][x], 1 + costs[y+1][x])
+                elif y + 1 >= H:
+                    costs[y][x] = min(costs[y][x], 1 + costs[y][x+1])
+                else:
+                    costs[y][x] = min(costs[y][x], 1 + min(costs[y][x+1], costs[y+1][x]))
+
+
+        q = [(-costs[0][0], 0, 0)]
+        seen = set()
         while q:
-            highest, x, y = heapq.heappop(q)
-            curr = min(-highest, self.safe[y][x])
-            if (x,y) == (N-1, N-1):
-                return curr
+            currCost, x, y = heapq.heappop(q)
+            if (x,y) == (W-1, H-1):
+                return -currCost
 
-            if (x,y) in seen:
-                continue
-            seen.add((x,y))         
-            for dx, dy in [(0,1), (1,0), (-1,0), (0, -1)]:
-                if 0 <= x + dx < N and 0 <= y + dy < N and grid[y + dy][x + dx] != 1:
-                    heapq.heappush(q,[-curr, dx + x, dy + y])
-        return 0
-    
+            for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
+                nX, nY = x + dx, y + dy
+                if 0 <= nX < W and 0 <= nY < H and (nX, nY) not in seen:
+                    seen.add((nX, nY))
+                    heapq.heappush(q, (-min(costs[nY][nX], -currCost), nX, nY))
+
+        return -1
+
+       
