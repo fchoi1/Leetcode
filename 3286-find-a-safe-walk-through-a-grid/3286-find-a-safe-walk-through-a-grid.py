36b0e91969d16715b591f0c801de785d34fe983e
Fab <42979283+fchoi1@users.noreply.github.com>
2026-07-01 22:18:56 -0400

    Time: 139 ms (49.53%), Space: 20.1 MB (27.36%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        # min cost to bottom
+        # dijstras
+
+        q = [(grid[0][0], (0,0))] # cost and x,y
+        seen = set()
+        W = len(grid[0])
+        H = len(grid)
+
+        while q:
+            cost, (x, y) = heappop(q)
+            if (x,y) in seen:
+                continue
+            seen.add((x,y))
+
+            if (x, y) == (W - 1, H - 1):
+                return health > cost
+
+            for dx, dy in [(0,1), (1,0), (-1, 0), (0, -1)]:
+                nx = x + dx
+                ny = y + dy
+
+                if 0 <= nx < W and 0 <= ny < H:
+                    if (nx, ny) not in seen:
+                        heappush(q, (cost + grid[ny][nx], (nx, ny)))
+        print("error")
+        return None
+
+
